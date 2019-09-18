f2f7f2a1e6fc9485596df2b51c4125dad3084ff6
chj <chjfth@gmail.com>
2019-09-18 17:10:31 +0800

    case3.2

---
+++ java/twoclass/case3.2/test/TimelineTest.java
@@ -0,0 +1,15 @@
+import mypkg1.*;
+
+public class TimelineTest {
+
+	public static void main(String[] args) {
+
+		Timeline timeline = new Timeline();
+		int expected = 5;
+
+		timeline.setFetchCount(expected);
+		int actual = timeline.getFetchCount();
+		
+		System.out.println("Fetch = "+actual);
+	}
+}
