e6d3a9a2549d9a59ed2e350e1fe49feafa78328a
GBchj <chjfth@gmail.com>
2019-12-19 11:10:58 +0800

    do_compile.bat : Show how to use resgen.exe from command line

---
+++ testRes/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace testRes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackgroundImage = global::testRes.Resource1.问号绿;
+            this.flowLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(38, 47);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(126, 117);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 204);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ testRes/do_compile.bat
@@ -0,0 +1,22 @@
+@REM  == Add resgen.exe to path
+PATH=C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools;%PATH%
+
+@REM == Use resgen.exe to generate .resources file.
+@REM == The output file content has nothing to do with output filename.
+@REM == But, the filename reflects the namespace of the .resource file which is critical.
+resgen.exe Resource1.resx testRes.Resource1.resources
+@if ERRORLEVEL 1 (
+    echo resgen execution Error!
+    exit /b 1
+)
+
+@set CSFILES=Form1.cs Form1.Designer.cs Program.cs Resource1.Designer.cs Properties\Settings.Designer.cs Properties\Resources.Designer.cs Properties\AssemblyInfo.cs
+
+@REM == Call csc.exe to compile .cs and link resources.
+csc.exe %CSFILES% /resource:Resource1.resources
+@if ERRORLEVEL 1 (
+    echo csc execution Error!
+    exit /b 1
+)
+
+@echo Compile success.
