e17c3b56076cc3fc8a8df958c6e1e0f7d3549b0e
GBchj <chjfth@gmail.com>
2019-12-24 11:18:03 +0800

    Modify Tab Index

--- CSharp/UIbyThread/Form1.Designer.cs
+++ CSharp/UIbyThread/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Location = new System.Drawing.Point(28, 130);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(160, 30);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 1;
             this.button1.Text = "UI direct";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -52,7 +52,8 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(398, 92);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
             this.textBox1.Text = "Set by code";
             // 
             // button2
@@ -60,7 +61,7 @@
             this.button2.Location = new System.Drawing.Point(233, 130);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(160, 30);
-            this.button2.TabIndex = 3;
+            this.button2.TabIndex = 2;
             this.button2.Text = "UI marshal";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -71,7 +72,8 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(365, 20);
-            this.textBox2.TabIndex = 4;
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TabStop = false;
             // 
             // Form1
             // 
@@ -83,7 +85,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "UIbyThread";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -92,8 +94,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
     }
