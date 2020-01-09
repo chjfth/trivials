ad119a6057f1f7864aab1641433c1a77ba24a24a
Jimm from chjm2016 <chjfth@gmail.com>
2020-01-09 10:11:57 +0800

    NavEvent OK
    (0) webBrowser1.Navigating
      e.Url = about:blank
      e.TargetFrameName =
    (0) webBrowser1.Navigated
      e.Url = about:blank
    (0) webBrowser1.Navigated
      e.Url = about:blank
    (0) webBrowser1.DocumentCompleted
      e.Url = about:blank
    (1) webBrowser1.Navigating
      e.Url = about:blank
      e.TargetFrameName =
    (1) webBrowser1.Navigated
      e.Url = about:blank
    (1) webBrowser1.Navigated
      e.Url = about:blank
    (1) webBrowser1.DocumentCompleted
      e.Url = about:blank

--- CSharp/IE_sln/NavEvent/Program.cs
+++ CSharp/IE_sln/NavEvent/Program.cs
@@ -11,6 +11,7 @@ public class Form1 : Form
 {
 	private WebBrowser webBrowser1 = new WebBrowser();
 	private Button button1 = new Button();
+	int count = 0;
 
 	[STAThread]
 	public static void Main()
@@ -21,18 +22,52 @@ public class Form1 : Form
 
 	public Form1()
 	{
+		this.Text = "Navigating Event demo";
 		this.Width = 360; // 360px width on a text-scale 100% monitor
 
-		button1.Text = "call script code from client code";
+		button1.Text = "Set a new webBrowser1.DocumentText";
 		button1.Dock = DockStyle.Top;
 		button1.Click += new EventHandler(button1_Click);
 
 		webBrowser1.Dock = DockStyle.Fill;
 
+		webBrowser1.Navigating += 
+			delegate (object sender, WebBrowserNavigatingEventArgs e)
+			{
+				Console.WriteLine($"({count}) webBrowser1.Navigating");
+                Console.WriteLine("  e.Url = " + e.Url);
+				Console.WriteLine("  e.TargetFrameName = " + e.TargetFrameName);
+			}; // C# 2.0 anonymous method
+
+		webBrowser1.Navigated +=
+			(object sender, WebBrowserNavigatedEventArgs e) =>
+			{
+				Console.WriteLine($"({count}) webBrowser1.Navigated");
+				Console.WriteLine("  e.Url = " + e.Url);
+			}; // C# 3.0 lambda expression
+
+		webBrowser1.DocumentCompleted +=
+			(sender, e) => // e is WebBrowserDocumentCompletedEventArgs 
+			{
+				Console.WriteLine($"({count}) webBrowser1.DocumentCompleted");
+				Console.WriteLine("  e.Url = " + e.Url);
+			}; // C# 3.0 lambda expression with implicit parameter type
+
 		this.Controls.Add(webBrowser1);
 		this.Controls.Add(button1);
 
 		this.Load += new EventHandler(Form1_Load);
+	}
+
+	void SetNewHtml()
+	{
+		webBrowser1.DocumentText =
+			"<html><head><script>" +
+			"function jstest(message) { alert(message); return 'JS Done.'; }" +
+			"</script></head><body><button " +
+			"onclick=\"window.external.csTest('called from script code')\">" +
+			$"({count}) call client code from script code</button>" +
+			"</body></html>";
 	}
 
 	private void Form1_Load(object sender, EventArgs e)
@@ -45,13 +80,7 @@ public class Form1 : Form
 		// Uncomment the following line when you are finished debugging.
 		//webBrowser1.ScriptErrorsSuppressed = true;
 
-		webBrowser1.DocumentText =
-			"<html><head><script>" +
-			"function jstest(message) { alert(message); return 'JS Done.'; }" +
-			"</script></head><body><button " +
-			"onclick=\"window.external.csTest('called from script code')\">" +
-			"call client code from script code</button>" +
-			"</body></html>";
+		SetNewHtml();
 	}
 
 	public void csTest(String message)
@@ -61,7 +90,8 @@ public class Form1 : Form
 
 	private void button1_Click(object sender, EventArgs e)
 	{
-		Console.WriteLine("hhhhhhhhhh");
+		count++;
+		SetNewHtml();
 	}
 
 }
