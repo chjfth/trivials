163beeb56fc445ff30acaa3dbc1f2df09ce7b5b1
Jimm chja20 <chjfth@gmail.com>
2020-03-29 23:45:32 +0800

    Update: print full-qualified name of foo2.
    CMD$ python  D:\gitw\trivials\python\testri\testfoo\testfoo2.py
    dirtoppkg = D:\gitw\trivials\python
    hellofoo.
    hellofoo2().
    Full-qualified name of foo2 is: testri.foo.foo2
    hellobar.

--- python/testri/foo/hellofoo.py
+++ python/testri/foo/hellofoo.py
@@ -4,6 +4,7 @@ from . import foo2
 def sayhello():
 	print("hellofoo.")
 	foo2.hellofoo2()
+	print("Full-qualified name of foo2 is: %s"%(foo2.__name__))
 	hellobar.sayhello()
 
 if __name__=='__main__':
