8337e97c0d16fcb6ec8dd38fdfffcd928065d478
Jimm chja20 <chjfth@gmail.com>
2020-04-05 00:29:07 +0800

    Tagging mypkg1d
      from . import *

--- python/test-relative-import/mypkg1/saysth/__init__.py
+++ python/test-relative-import/mypkg1/saysth/__init__.py
@@ -1,6 +1,6 @@
 
-__all__ = ["sayhello", "sayhowdy", "dumbit"] # mypkg1c
+__all__ = ["sayhello", "sayhowdy", "dumbit"] # mypkg1d
 
 print("[%s] __name__=%s"%(__file__,__name__))
 
-from . import sayhello, sayhowdy
+from . import *
