e331a5c9521fa28569073bb85f9b8f0c96ff0e6d
Jimm chja20 <chjfth@gmail.com>
2020-04-05 10:52:40 +0800

    add say_all.py to call sayhello() and sayhowdy()

---
+++ python/test-relative-import/mypkg1/saysth/say_all.py
@@ -0,0 +1,13 @@
+
+print("[%s] __name__=%s"%(__file__,__name__))
+
+from . import * # * is determined by __init__.py -> __all__[] list
+
+def say_all():
+	sayhello()
+	sayhowdy()
+
+# This would cause double-import warning!
+if __name__=='__main__':
+	say_all()
+
