24efeebe56926c53b43b9c87e94780814ef7cd2f
GBchj <chjfth@gmail.com>
2020-04-07 18:52:49 +0800

    fix to prev: Use forfiles.exe to get seconds-precision file time.

--- bat/compare-filetime.bat
+++ bat/compare-filetime.bat
@@ -6,16 +6,15 @@ REM https://stackoverflow.com/questions/1687014/how-do-i-compare-timestamps-of-f
 REM Demo: Create a.txt bb.txt c.txt with increasing timestamp, see the compare result
 echo Creating a.txt
 echo a > a.txt
-call :DelaySeconds 3
+call :DelaySeconds 1
 
 echo Creating b.txt
 echo b > b.txt
-call :DelaySeconds 3
+call :DelaySeconds 1
 
 echo Creating c.txt
 echo c > c.txt
 
-echo 999999999999
 
 set file1=a.txt
 set file2=b.txt
@@ -34,24 +33,19 @@ goto :eof
 SETLOCAL
   SET _file1=%1
   SET _file2=%2
-echo !!! %file1% , %file2%
-  FOR %%i IN (%_file1%) DO SET DATE1=%%~ti
-  FOR %%i IN (%_file2%) DO SET DATE2=%%~ti
+  for /F "tokens=*" %%a in ('forfiles /m %_file1% /c "cmd /c echo @fdate @ftime"') do set DATE1=%%a
+  for /F "tokens=*" %%a in ('forfiles /m %_file2% /c "cmd /c echo @fdate @ftime"') do set DATE2=%%a
   IF "%DATE1%"=="%DATE2%" (
-echo 0000 ["%DATE1%"=="%DATE2%"]
     set _result=no
     goto :END_IsFileNewer
   )
 
-DIR /O:D %_file1% %_file2%
   FOR /F %%i IN ('DIR /B /O:D %_file1% %_file2%') DO SET _newer=%%i
   if "%_newer%"=="%_file1%" (
-echo yyyy
     set _result=yes
     goto :END_IsFileNewer
   )
 :END_IsFileNewer  
-echo ========%_result%
 ENDLOCAL & SET _ret=%_result%
 exit /b
 
