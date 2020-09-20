bc0a8f558ccbee1f5423c1e12c85147f7d47e755
Jimm chja20 <chjfth@gmail.com>
2020-09-20 22:21:29 +0800

    dulog: Demonstrate the concept of my dual-log idea.

---
+++ python/dulog/dulog.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import os, sys, time
+
+class ChsFile:
+	
+	class Err(Exception):
+		def __init__(self, msg):
+			self.msg = msg
+
+	def __init__(self, filepath):
+		self.filepath = filepath
+
+	def GetText(self):
+		with open(self.filepath, "r", encoding="utf8") as fh:
+			try:
+				text = fh.read()
+				return text
+			except UnicodeDecodeError:
+				pass
+		
+		with open(self.filepath, "r", encoding="gbk") as fh:
+			try:
+				text = fh.read()
+				return text
+			except UnicodeDecodeError:
+				pass
+		
+		raise ChsFile.Err("The file is in neither UTF8 or GBK.")
+	
+
+if __name__=='__main__':
+	print("Python version: {}.{}.{}".format(*sys.version_info[0:3]))
+	if len(sys.argv)==1:
+		print("Input a file name as parameter. The file should be a UTF8 or GBK text file.")
+		exit(1)
+	
+	filepath = sys.argv[1]
+	
+	try:
+		chsfile = ChsFile(filepath)
+		chstext = chsfile.GetText()
+		print(chstext)
+	except ChsFile.Err as e:
+		print("[ERROR] "+e.msg)
