3d77e3f133c8256fd589708bf2793c1ad7c6af13
Jimm chja20 <chjfth@gmail.com>
2020-09-20 23:44:43 +0800

    created .ChsFile.log log-files now.

--- python/dulog/dulog.py
+++ python/dulog/dulog.py
@@ -3,14 +3,42 @@
 
 import os, sys, time
 
+def nowtimestr():
+	return time.strftime('%Y%m%d.%H%M%S', time.localtime())
+
+start_ts = nowtimestr()
+
 class ChsFile:
+	
+	logfilenam = start_ts + ".ChsFile.log"
+	logfiledir = "__logs__"
+	logfh = None
 	
 	class Err(Exception):
 		def __init__(self, msg):
 			self.msg = msg
 
+	@classmethod
+	def CreateModuleLogfile(cls):
+		if not cls.logfh:
+			try:
+				logfilepath = os.path.join(cls.logfiledir, cls.logfilenam)
+				os.makedirs(cls.logfiledir, exist_ok=True)
+				cls.logfh = open(logfilepath, 'w', encoding='utf8')
+			except OSError:
+				raise cls.Err("Cannot create logfile {}".format(logfilepath))
+	
+	@classmethod
+	def mlog(cls, msg, isprint=False):
+		line = "[{}]{}\n".format(nowtimestr(), msg)
+		assert(cls.logfh)
+		cls.logfh.write(line)
+		if isprint:
+			print(line)
+
 	def __init__(self, filepath):
-		self.filepath = filepath
+		__class__.CreateModuleLogfile() # Create ChsFile-module logfile first
+		self.filepath = filepath        # Record protagonist filepath
 
 	def GetText(self):
 		with open(self.filepath, "r", encoding="utf8") as fh:
@@ -18,6 +46,7 @@ class ChsFile:
 				text = fh.read()
 				return text
 			except UnicodeDecodeError:
+				__class__.mlog("Not UTF8: {}".format(self.filepath))
 				pass
 		
 		with open(self.filepath, "r", encoding="gbk") as fh:
@@ -25,9 +54,10 @@ class ChsFile:
 				text = fh.read()
 				return text
 			except UnicodeDecodeError:
+				__class__.mlog("Not GBK : {}".format(self.filepath))
 				pass
 		
-		raise ChsFile.Err("The file is in neither UTF8 or GBK.")
+		raise __class__.Err("The file is in neither UTF8 or GBK.")
 	
 	def GetBoth(self):
 		filebytes = os.path.getsize(self.filepath)
