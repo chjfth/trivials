96e824e49a976a35747168e45fd25167998fb1ae
Jimm Chen crtek <chjfth@gmail.com>
2020-12-16 18:18:53 +0800

    minor more app.route()

--- python/flask/simple1/simple1.py
+++ python/flask/simple1/simple1.py
@@ -4,6 +4,9 @@ from flask import make_response
 
 app = Flask(__name__)
 
+tzchina = datetime.timezone(datetime.timedelta(hours=8)) 
+dt_expire = datetime.datetime(2020, 12, 20, 9,2,3, 0, tzchina)
+
 @app.route('/')
 def index():
 	return '<h1>Hello World !</h1>'
@@ -23,27 +26,40 @@ def cookie0():
 @app.route('/cookie1')
 def cookie1():
 
-
-	tzchina = datetime.timezone(datetime.timedelta(hours=8)) 
-	dt_expire = datetime.datetime(2020, 12, 20, 9,2,3, 0, tzchina)
-
 	resp = make_response('<p>Persistent cookies set.</p>')
 	
-	resp.set_cookie('mycook', 'yummy', expires=dt_expire)
+	resp.set_cookie('mycook_shortlive', 'short-lived', expires=dt_expire)
 	
-	resp.set_cookie('mycook_with_path1', '1path', expires=dt_expire,
+	resp.set_cookie('mycook_persist', 'yum0', expires=dt_expire)
+	
+	resp.set_cookie('mycook_path_cookie1', 'yum1', expires=dt_expire,
 		path='/cookie1')
 	#
-	resp.set_cookie('mycook_with_path2', '2path', expires=dt_expire,
-		path='/mypath')
+	resp.set_cookie('mycook_path_cookie2', 'yum2', expires=dt_expire,
+		path='/cookie2')
 	
-	resp.set_cookie('mycook_with_domain', '3domain', expires=dt_expire,
-		domain='.chjhost.com')
-	
-	resp.set_cookie('mycook_httponly', '4httponly', expires=dt_expire,
+	resp.set_cookie('mycook_httponly', 'yum3', expires=dt_expire,
 		httponly=True)
 	
-	resp.set_cookie('mycook_secure', '5secure', expires=dt_expire,
+	resp.set_cookie('mycook_secure', 'yum4', expires=dt_expire,
 		secure=True)
 	
+	resp.set_cookie('mycook_with_domain_attr', 'yum5', expires=dt_expire,
+		domain='.chjwork.com')
+	
+	return resp
+
+@app.route('/cookie4')
+def cookie4():
+	resp = make_response('<p>Setting weird cookies.</p>')
+	resp.set_cookie('Normal', 'NormalVal')
+	resp.set_cookie('NormalWithSpace', 'Normal Value')
+	resp.set_cookie('WithComma', 'abc,def')
+	resp.set_cookie('WithSemicolon', 'xxx;yyy')
+	return resp
+	
+@app.route('/cookie5')
+def cookie5():
+	resp = make_response('<p>Meet cookie5.</p>')
+	resp.set_cookie('Meet5', 'Yum5', expires=dt_expire, domain='chjhost.com:5000')
 	return resp
