2a0100df06fd74e5316ed288c13ee2230114fa22
JimmChen vbwin7 <chjfth@gmail.com>
2020-12-31 10:34:02 +0800

    Add p581, p591 code

--- CSharp/csTask/csnutsTask1/Program.cs
+++ CSharp/csTask/csnutsTask1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,6 +12,8 @@ namespace csnutsTask1
 {
     class Program
     {
+        #region Logger
+        static readonly object _locker = new object();
         static DateTime s_last_DateTime = new DateTime(0);
         //
         static string text_elapse(int millisec)
@@ -25,18 +28,22 @@ namespace csnutsTask1
         //
         static void log(string s)
         {
-            DateTime nowdt = DateTime.Now;
-            int millisec_gap = (int) (nowdt - s_last_DateTime).TotalMilliseconds;
-            int seconds_gap = millisec_gap/1000;
-            if (s_last_DateTime.Ticks > 0 && seconds_gap > 0)
+            lock(_locker)
             {
-                Console.Out.WriteLine("".PadLeft(Math.Min(10, seconds_gap), '.'));
+                DateTime nowdt = DateTime.Now;
+                int millisec_gap = (int) (nowdt - s_last_DateTime).TotalMilliseconds;
+                int seconds_gap = millisec_gap / 1000;
+                if (s_last_DateTime.Ticks > 0 && seconds_gap > 0)
+                {
+                    Console.Out.WriteLine("".PadLeft(Math.Min(10, seconds_gap), '.'));
+                }
+
+                string tsprefix = string.Format("[{0:D2}:{1:D2}:{2:D2}.{3:D3}{4}]",
+                    nowdt.Hour, nowdt.Minute, nowdt.Second, nowdt.Millisecond, text_elapse(millisec_gap)
+                );
+                Console.Out.WriteLine($"{tsprefix}{s}");
+                s_last_DateTime = nowdt;
             }
-            string tsprefix = string.Format("[{0:D2}:{1:D2}:{2:D2}.{3:D3}{4}]",
-                nowdt.Hour, nowdt.Minute, nowdt.Second, nowdt.Millisecond, text_elapse(millisec_gap)
-            );
-            Console.Out.WriteLine($"{tsprefix}{s}");
-            s_last_DateTime = nowdt;
         }
 
         static string tid()
@@ -48,6 +55,20 @@ namespace csnutsTask1
         {
             log($"{tid()} {s}");
         }
+        #endregion
+
+        private static int s_mainthread_tid = AppDomain.GetCurrentThreadId();
+
+        static void AssertMainThread(bool b)
+        {
+            int tid = AppDomain.GetCurrentThreadId();
+            if (b)
+                Debug.Assert(s_mainthread_tid == tid);
+            else
+                Debug.Assert(s_mainthread_tid != tid);
+        }
+
+        ////////
 
         static void p579_CountPrimes()
         {
@@ -80,8 +101,11 @@ namespace csnutsTask1
             var awaiter = primeNumberTask.GetAwaiter();
             awaiter.OnCompleted(() =>
             {
-                // 不妨把这个回调函数体叫做 conti, 它的前情任务, 书中称之 antecedent task (antetask).
-                logtid($"Task Conti executing...");
+                // This block of code is called Continuation(my abbr: Conti),
+                // and its related Task is called antecedent task (my abbr: Antetask).
+                logtid($"Conti executing...");
+
+                AssertMainThread(false); // due to lack of Synchronization-context
 
                 int result = awaiter.GetResult();
                 Console.WriteLine("p581 answer is " + result); // Writes result
@@ -92,17 +116,41 @@ namespace csnutsTask1
             return primeNumberTask;
         }
 
+        static Task<int> GetPrimesCountAsync(int start, int count) // p591
+        {
+            return Task.Run(() =>
+                ParallelEnumerable.Range(start, count).Count(n =>
+                    Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+        }
+
+        static async void p591_Await()
+        {
+            string funcname = "p591_Await()";
+            logtid($"{funcname} Start.");
+
+            Console.Out.WriteLine("ChjNote: System.Reflection.MethodBase.GetCurrentMethod().Name=" +
+                                  System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            int result = await GetPrimesCountAsync(2, 1000000);
+
+            logtid($"{funcname} Resume from await.");
+            Console.WriteLine("p591 answer is " + result);
+
+            logtid($"{funcname} End.");
+        }
+
         static void Main(string[] args)
         {
             p579_CountPrimes();
 
             Task<int> task = null;
             task = p581_AwaiterConti();
-
             task.Wait();
 
+            p591_Await();
+
             log("Main thread final Sleep then quit.");
-            Thread.Sleep(500);
+            Thread.Sleep(3000);
         }
     }
 }
