f5aa0bcbc285c39aeab0209f4f675200f2176421
JimmChen vbwin7 <chjfth@gmail.com>
2021-01-03 11:16:47 +0800

    p607-608 cancel tests.

--- CSharp/csTask/csCancelTask/Program.cs
+++ CSharp/csTask/csCancelTask/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace csCancelTask
 {
-    class CancellationToken
+    class MyCancellationToken
     {
         public bool IsCancellationRequested { get; private set; }
 
@@ -77,8 +79,10 @@ namespace csCancelTask
         }
         #endregion
 
-        async static Task p606_FooAsync(CancellationToken cancellationToken)
+        async static Task p606_FooAsync(MyCancellationToken cancellationToken)
         {
+            Thread.Sleep(200); // Chj: to tear apart 1000ms boundary
+
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(i);
@@ -89,10 +93,10 @@ namespace csCancelTask
 
         async static Task p606_LaunchFoo_and_TimedCancel()
         {
-            var token = new CancellationToken();
+            var token = new MyCancellationToken();
 
-            Task.Delay(2000).ContinueWith(
-                ant => token.Cancel()
+            Task.Delay(2000).ContinueWith(ant => 
+                token.Cancel()
                 );   // Tell it to cancel in two seconds.
 
             await p606_FooAsync(token);
@@ -112,14 +116,101 @@ namespace csCancelTask
             catch (OperationCanceledException e)
             {
                 logtid($"OK. OperationCanceledException caught. We know that execution of p606_FooAsync() has been canceled.");
+
+                Debug.Assert(e.CancellationToken.IsCancellationRequested == false); // bcz we're not using System's CancellationToken type.
             }
 
             logtid($"{funcname} End.");
         }
 
+        ////
+
+        async static Task p607_FooAsync(CancellationToken cancellationToken)
+        {
+            Thread.Sleep(200); // Chj: to tear apart 1000ms boundary
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                await Task.Delay(1000, cancellationToken);
+            }
+        }
+
+        async static Task p607_LaunchFoo_and_TimedCancel()
+        {
+            var cancelSource = new CancellationTokenSource();
+
+            Task.Delay(2000).ContinueWith(ant => 
+                cancelSource.Cancel()
+                );   // Tell it to cancel in two seconds.
+
+            await p607_FooAsync(cancelSource.Token);
+        }
+
+        static void p607_TestCancel()
+        {
+            string funcname = System.Reflection.MethodBase.GetCurrentMethod().Name + "()";
+            logtid($"{funcname} Start.");
+
+            Task task = p607_LaunchFoo_and_TimedCancel();
+
+            try
+            {
+                task.GetAwaiter().GetResult();
+            }
+            catch (OperationCanceledException e)
+            {
+                logtid($"OK. OperationCanceledException caught. We know that execution of p607_FooAsync() has been canceled.");
+
+                Debug.Assert(e.CancellationToken.IsCancellationRequested==true);
+            }
+
+            logtid($"{funcname} End.");
+        }
+
+        ////
+
+        async static Task p608_LaunchFoo_AutoTimedCancel()
+        {
+            int seconds = 5;
+            logtid($"Set CancellationTokenSource to auto cancel in {seconds} seconds.");
+
+            var cancelSource = new CancellationTokenSource(seconds*1000);   // This tells it to cancel in 5 seconds
+            await p607_FooAsync(cancelSource.Token); // yes, same as p607
+        }
+
+        static void p608_TestAutoTimedCancel()
+        {
+            string funcname = System.Reflection.MethodBase.GetCurrentMethod().Name + "()";
+            logtid($"{funcname} Start.");
+
+            Task task = p608_LaunchFoo_AutoTimedCancel();
+
+            try
+            {
+                task.GetAwaiter().GetResult();
+            }
+            catch (OperationCanceledException e)
+            {
+                logtid($"OK. OperationCanceledException caught.");
+
+                Debug.Assert(e.CancellationToken.IsCancellationRequested == true);
+            }
+
+            logtid($"{funcname} End.");
+        }
+
+        ////
+
         static void Main(string[] args)
         {
             p606_TestHandCraftedCancel();
+
+            Console.Out.WriteLine("");
+            p607_TestCancel();
+
+            Console.Out.WriteLine("");
+            p608_TestAutoTimedCancel();
         }
     }
 }
