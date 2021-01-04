ddf90d51d7cc41d88911af66d83ca0b336153d0e
JimmChen vbwin7 <chjfth@gmail.com>
2021-01-04 08:48:42 +0800

    Orig code from //Async in C# 5.0//
    Downloaded from
    https://web.archive.org/web/20140801221502/https://bitbucket.org/alexdavies74/faviconbrowser

---
+++ CSharp/ASYNCS5/FaviconBrowser/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace FaviconBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly List<string> s_Domains = new List<string>
+                                                             {
+                                                                 "google.com",
+                                                                 "bing.com",
+                                                                 "oreilly.com",
+                                                                 "simple-talk.com",
+                                                                 "microsoft.com",
+                                                                 "facebook.com",
+                                                                 "twitter.com",
+                                                                 "reddit.com",
+                                                                 "baidu.com",
+                                                                 "bbc.co.uk"
+                                                             };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (string domain in s_Domains)
+            {
+                AddAFavicon(domain);
+            }
+        }
+
+        private void AddAFavicon(string domain)
+        {
+            WebClient webClient = new WebClient();
+            byte[] bytes = webClient.DownloadData("http://" + domain + "/favicon.ico");
+            Image imageControl = MakeImageControl(bytes);
+            m_WrapPanel.Children.Add(imageControl);
+        }
+
+        private static Image MakeImageControl(byte[] bytes)
+        {
+            Image imageControl = new Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(bytes);
+            bitmapImage.EndInit();
+            imageControl.Source = bitmapImage;
+            imageControl.Width = 16;
+            imageControl.Height = 16;
+            return imageControl;
+        }
+    }
+}
