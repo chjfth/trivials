c297bed3439bed75197fcd5fff73457f03e6cacc
JimmChen vbwin7 <chjfth@gmail.com>
2021-01-04 13:50:21 +0800

    ASYNCS5 - add ch03-manual

---
+++ CSharp/ASYNCS5/ch03-manual/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace FaviconBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly List<string> s_Domains = new List<string>
+        {
+            //"google.com",
+            "www.apple.com",
+            "bing.com",
+            "oreilly.com",
+            //"simple-talk.com",
+            "microsoft.com",
+            //"facebook.com",
+            //"twitter.com",
+            //"reddit.com",
+            "baidu.com",
+            //"bbc.co.uk",
+            "www.amazon.com",
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                                                   | SecurityProtocolType.Tls11 // require .NET 4.5
+                                                   | SecurityProtocolType.Tls12 // require .NET 4.5
+                                                   | SecurityProtocolType.Ssl3;
+            // [2021-01-04] Chj:
+            // http://www.oreilly.com/favicon.ico will be automatically redirected to
+            // https://www.oreilly.com/favicon.ico , which requires Tls11, Tls12 .
+        }
+
+        private void GetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (string domain in s_Domains)
+            {
+                AddAFavicon(domain);
+            }
+        }
+
+        private void AddAFavicon(string domain)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadDataCompleted += OnWebClientOnDownloadDataCompleted;
+            webClient.DownloadDataAsync(new Uri("http://" + domain + "/favicon.ico"));
+        }
+
+        private void OnWebClientOnDownloadDataCompleted(object sender, DownloadDataCompletedEventArgs args)
+        {
+            Image imageControl = MakeImageControl(args.Result);
+            m_WrapPanel.Children.Add(imageControl);
+        }
+
+        private static Image MakeImageControl(byte[] bytes)
+        {
+            Image imageControl = new Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(bytes);
+            bitmapImage.EndInit();
+            imageControl.Source = bitmapImage;
+            imageControl.Width = 16;
+            imageControl.Height = 16;
+            return imageControl;
+        }
+    }
+}
