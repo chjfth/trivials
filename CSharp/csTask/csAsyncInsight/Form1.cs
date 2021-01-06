d913959fc196a66cfe4acb2bb726f79e2d174e2c
JimmChen vbwin7 <chjfth@gmail.com>
2021-01-06 16:07:45 +0800

    An Winform-base program skeleton based on Lantian's code template.

--- CSharp/csTask/csAsyncInsight/Form1.cs
+++ CSharp/csTask/csAsyncInsight/Form1.cs
@@ -4,7 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +17,88 @@ namespace csAsyncInsight
 {
     public partial class Form1 : Form
     {
+        private static object locker = new object();
         public Form1()
         {
             InitializeComponent();
         }
+
+        #region Logger
+        private static int s_mainthread_tid = AppDomain.GetCurrentThreadId();
+
+        static readonly object _locker = new object();
+        static DateTime s_last_DateTime = new DateTime(0);
+        //
+        static string text_elapse(int millisec)
+        {
+            if (millisec >= 1000)
+            {
+                double sec = (double)millisec / 1000;
+                return $"(+{sec:g}s)";
+            }
+            else if (millisec > 0) return $"(+{millisec}ms)";
+            else return "";
+        }
+        //
+        private void log(string s)
+        {
+            lock (_locker)
+            {
+                DateTime nowdt = DateTime.Now;
+                int millisec_gap = (int)(nowdt - s_last_DateTime).TotalMilliseconds;
+                int seconds_gap = millisec_gap / 1000;
+                if (s_last_DateTime.Ticks > 0 && seconds_gap > 0)
+                {
+                    Console.Out.WriteLine("".PadLeft(Math.Min(10, seconds_gap), '.'));
+                }
+
+                string tsprefix = string.Format("[{0:D2}:{1:D2}:{2:D2}.{3:D3}{4,-10}] ",
+                    nowdt.Hour, nowdt.Minute, nowdt.Second, nowdt.Millisecond, text_elapse(millisec_gap)
+                );
+                s_last_DateTime = nowdt;
+
+                textBox1.BeginInvoke(new Action(() =>
+                {
+                    textBox1.AppendText($"{tsprefix}{s}" + Environment.NewLine);
+                }));
+            }
+        }
+
+        static string tid()
+        {
+            int tidnow = AppDomain.GetCurrentThreadId();
+            if (tidnow == s_mainthread_tid)
+                return "<tid=main>";
+            else
+                return $"<tid={tidnow}>";
+        }
+
+        void logtid(string s)
+        {
+            log(string.Format("{0,-11} {1}", tid(), s));
+        }
+        #endregion
+
+
+        private void Log(string s)
+        {
+            textBox1.BeginInvoke(new Action(() =>
+            {
+                textBox1.AppendText(s + Environment.NewLine);
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Log("btn1-click");
+            logtid("btn1-click");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Log("Form1-load");
+            logtid("Form1-load");
+        }
+
     }
 }
