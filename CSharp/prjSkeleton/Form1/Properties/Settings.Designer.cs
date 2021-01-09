7ec800e84539f0f6d334702d04c68510b5b4de6f
JimmChen vbwin7 <chjfth@gmail.com>
2021-01-09 09:37:18 +0800

    Change .NETFX version to 4.7

--- CSharp/prjSkeleton/Form1/Properties/Settings.Designer.cs
+++ CSharp/prjSkeleton/Form1/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace csAsyncInsight.Properties {
+namespace prjSkeleton.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
