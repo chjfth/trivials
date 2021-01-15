cc7ddb1b56bb1b486fa723cae0f8d01fef36cc78
Jimm Chen crtek <chjfth@gmail.com>
2021-01-15 11:54:09 +0800

    p40

---
+++ python/UASYNCPY/CH3/p40-future.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import asyncio
+
+async def main(f: asyncio.Future):
+	await asyncio.sleep(1)
+	f.set_result('I have finished.')
+
+loop = asyncio.get_event_loop()
+fut = asyncio.Future()
+print(fut.done()) # False
+
+coro = main(fut)
+tsk = loop.create_task(coro)
+# <Task pending name='Task-1' coro=<main() running at <console>:1>>
+
+loop.run_until_complete(fut)
+# 'I have finished.'
+
+print(fut.done()) # True
