8d82a236835c657d7f6adf2bfb543af742d0b40c
Jimm Chen crtek <chjfth@gmail.com>
2021-01-15 10:17:41 +0800

    minor update

--- python/UASYNCPY/CH3/p35-cancel.py
+++ python/UASYNCPY/CH3/p35-cancel.py
@@ -4,14 +4,19 @@
 import asyncio
 
 async def f():
+	count = 0
 	try:
 		while True: 
 			await asyncio.sleep(0)
+			count += 1
+			print("[%d]Code run after await."%(count))
 	except asyncio.CancelledError:
 		print('I was cancelled!')
 	else:
 		return 111
+
 coro = f()
 coro.send(None)
 coro.send(None)
+coro.send(10)
 coro.throw(asyncio.CancelledError)
