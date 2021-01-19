8ad24fc844c4328dfae22213f0e621d2fd002aff
Jimm Chen crtek <chjfth@gmail.com>
2021-01-19 12:32:17 +0800

    experiment code to make  http_get(url) throw exception on 'JP'

--- python/_flask/FLPY-ch17-serv/serv1.py
+++ python/_flask/FLPY-ch17-serv/serv1.py
@@ -14,14 +14,35 @@ dt_expire = datetime(2020, 12, 30, 9,2,3, 0, tzchina)
 def index():
 	return '<h1>Demo server for [FLPY] CH 17!</h1>'
 
-@app.route('/data/flags/<cc1>/<cc2>.gif')
-def serv_flag(cc1, cc2):
+@app.route('/data/flags/<cc1>/<filename>')
+def serv_flag(cc1, filename):
 	time.sleep(1.0) # 1 second sleep to simulate network delay
 	
-	if cc1.upper()=='US':
-		abort(404)
-	
-	return "This is {}.gif".format(cc1)
+	if filename.endswith('.gif'):
+
+		if cc1.upper()=='US': # simulate error
+			abort(404)
+
+		return "This is {}.gif".format(cc1)
+		
+	elif filename.endswith('.json'):
+		
+		body = """"tld_cc": ".{cc1}", 
+			"iso_cc": "{cc1}", 
+			"country": "Country name of {cc1}", 
+			"gec_cc": "gec of {cc1}"
+			""".format(cc1=cc1)
+		body = "{" + body + "}"
+
+		if cc1.upper()=='JP': # simulate error
+			body = "MY WRONG JSON BODY"
+
+		resp = make_response(body)
+		resp.mimetype = "text/json"
+		
+		return resp
+	else:
+		return "What do you want?"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
