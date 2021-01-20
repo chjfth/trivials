df8fbcbcc475244195e97aee96448b0da0b4e724
JimmChen vbwin7 <chjfth@gmail.com>
2021-01-20 19:12:49 +0800

    Refactor code into Utils.WebRequest_TaskTimeout(), good.

--- CSharp/AsyncHttp/Form1/AsyncHttp.cs
+++ CSharp/AsyncHttp/Form1/AsyncHttp.cs
@@ -108,63 +108,28 @@ namespace ZjbLib
             // so we use a Delay Task to help implement the timeout. Works very well.
 
             Task<WebResponse> tskWeb = _webreq.GetResponseAsync();
-
-            Task tskCompleted = await Task.WhenAny(tskWeb, tskTimeout);
-
-            if (tskCompleted == tskWeb)
+            WebResponse webresp = await Utils.WebRequest_TaskTimeout(_webreq, tskWeb, ct, timeout_millisec);
+            using (webresp)
             {
-                WebResponse webresp = tskWeb.Result;
-                using (webresp)
-                {
-                    // Get HTTP response body Stream object, so that we can read the response body
-                    // piece by piece from the Stream object.
-                    // Note: There is no API called WebResponse.GetResponseStreamAsync(), bcz 
-                    // GetResponseStream() never block, and slow reading of http body is deferred to
-                    // stream reading.
+                // Get HTTP response body Stream object, so that we can read the response body
+                // piece by piece from the Stream object.
+                // Note: There is no API called WebResponse.GetResponseStreamAsync(), bcz 
+                // GetResponseStream() never block, and slow reading of http body is deferred to
+                // stream reading.
 
-                    Stream httpbodys = webresp.GetResponseStream();
+                Stream httpbodys = webresp.GetResponseStream();
 
-                    // Since we cannot know in advance how long the http body is, especially for
-                    // "chunked" transferring, so we need a temp stream as our own buffer.
-                    MemoryStream tempbodys = new MemoryStream();
+                // Since we cannot know in advance how long the http body is, especially for
+                // "chunked" transferring, so we need a temp stream as our own buffer.
+                MemoryStream tempbodys = new MemoryStream();
 
-                    Task tskRecvBody = httpbodys.CopyToAsync(tempbodys, 81920, ct);
 
-                    tskCompleted = await Task.WhenAny(tskRecvBody, tskTimeout);
-
-                    if (tskCompleted == tskRecvBody)
-                    {
-                        _respbody_bytes = tempbodys.GetBuffer();
-                        return _respbody_bytes;
-                    }
-                    else
-                    {
-                        // Receiving body timeout
-                        _webreq.Abort();
-                        await tskRecvBody; 
-
-                        // todo: Save partial recv body
-                        _respbody_bytes = tempbodys.GetBuffer();
-                        return null;
-                    }
-                }
+                Task tskRecvBody = httpbodys.CopyToAsync(tempbodys, 81920, ct);
+                await Utils.WebRequest_TaskTimeout(_webreq, tskRecvBody, ct, timeout_millisec);
+                
+                _respbody_bytes = tempbodys.GetBuffer();
+                return _respbody_bytes;
             }
-            else
-            {
-// TODO: add logging facility to know the detailed timing
-                // Timeout. So we need to cancel(Abort) our "true" Task in turn.
-
-                // Hope this aborts EVERYTHING, inc DNS resolving, TCP connect,
-                // SSL certificate verification etc.
-                _webreq.Abort();
-
-                // The calm down may not be instant, so we still need to wait for true 
-                // WebRequest task finish.
-                await tskWeb;
-
-                return null;
-            }
-
         }
 
         #region AUX
--- CSharp/AsyncHttp/Form1/helper.cs
+++ CSharp/AsyncHttp/Form1/helper.cs
@@ -16,8 +16,77 @@ namespace ZjbLib
     /// </summary>
     static partial class Utils
     {
-//        public static 
+        /// <summary>
+        /// For any WebRequest-related Task that does not support timeout or cancelling,
+        /// this encapsulation makes it capable of timeout and cancelling. Great idea.
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="webreq"></param>
+        /// <param name="tskOngoing"></param>
+        /// <param name="ct"></param>
+        /// <param name="timeout_millisec"></param>
+        /// <returns>The same meaning as tskOngoing's original semantics.
+        /// Return TResult value on success, or throw an exception on failure.</returns>
+        public async static Task<TResult> WebRequest_TaskTimeout<TResult>(
+            WebRequest webreq, 
+            Task<TResult> tskOngoing,
+            CancellationToken ct,
+            int timeout_millisec
+        )
+        {
+            // TResult maybe: WebRespone, ...
+
+            Task tskTimeout = Task.Delay(TimeSpan.FromMilliseconds(timeout_millisec), ct);
+
+            Task tskCompleted = await Task.WhenAny(tskOngoing, tskTimeout);
+
+            if (tskCompleted == tskOngoing)
+            {
+                return tskOngoing.Result;
+            }
+            else
+            {
+                // Timeout. So we need to cancel(abort) our "true" Task in turn.
+
+                // Hope this aborts EVERYTHING, inc DNS resolving, TCP connect,
+                // SSL certificate verification etc.
+                webreq.Abort();
+
+                // We still need to wait for the true result of the webreq.
+                // This may give our success result,
+                // or an WebException with WebExceptionStatus.RequestCanceled .
+                TResult result = await tskOngoing;
+                return result;
+            }
+        }
+
+        public async static Task WebRequest_TaskTimeout(
+            WebRequest webreq,
+            Task tskOngoing,
+            CancellationToken ct,
+            int timeout_millisec
+        )
+        {
+            //[2020-01-20] Q: Can I avoid duplicating the generic WebRequest_TaskTimeout<TResult>'s code?
+
+            Task tskTimeout = Task.Delay(TimeSpan.FromMilliseconds(timeout_millisec), ct);
+
+            Task tskCompleted = await Task.WhenAny(tskOngoing, tskTimeout);
+
+            if (tskCompleted == tskOngoing)
+            {
+            }
+            else
+            {
+                webreq.Abort();
+                await tskOngoing;
+            }
+            return;
+        }
+
     }
+
+
 
     /// <summary>
     /// MessageBox Utils
