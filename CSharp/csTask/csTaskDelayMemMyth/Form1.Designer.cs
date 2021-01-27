2ce39e15b45e3b69b96e84820f4a6e5f487aa08a
JimmChen vbwin7 <chjfth@gmail.com>
2021-01-27 14:50:10 +0800

    csTaskDelayMemMyth : See how does an un-cancelled Task consumes memory.

---
+++ CSharp/csTask/csTaskDelayMemMyth/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace prjSkeleton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnStartWithCancel = new System.Windows.Forms.Button();
+            this.btnStartNoCancel = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 488);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(150, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(650, 488);
+            this.panel3.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(650, 488);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnStartWithCancel);
+            this.panel2.Controls.Add(this.btnStartNoCancel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(150, 488);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnStartWithCancel
+            // 
+            this.btnStartWithCancel.Location = new System.Drawing.Point(12, 57);
+            this.btnStartWithCancel.Name = "btnStartWithCancel";
+            this.btnStartWithCancel.Size = new System.Drawing.Size(132, 25);
+            this.btnStartWithCancel.TabIndex = 1;
+            this.btnStartWithCancel.Text = "Start Tasks with &Cancel";
+            this.btnStartWithCancel.UseVisualStyleBackColor = true;
+            this.btnStartWithCancel.Click += new System.EventHandler(this.btnStartWithCancel_Click);
+            // 
+            // btnStartNoCancel
+            // 
+            this.btnStartNoCancel.Location = new System.Drawing.Point(12, 12);
+            this.btnStartNoCancel.Name = "btnStartNoCancel";
+            this.btnStartNoCancel.Size = new System.Drawing.Size(132, 25);
+            this.btnStartNoCancel.TabIndex = 0;
+            this.btnStartNoCancel.Text = "&Start Tasks no Cancel";
+            this.btnStartNoCancel.UseVisualStyleBackColor = true;
+            this.btnStartNoCancel.Click += new System.EventHandler(this.btnStartNoCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 488);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "csTaskDelayMemMyth";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnStartNoCancel;
+        private System.Windows.Forms.Button btnStartWithCancel;
+    }
+}
+
---
+++ CSharp/csTask/csTaskDelayMemMyth/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjSkeleton
+{
+    public partial class Form1 : Form
+    {
+        #region Logger
+
+        static int get_thread_id() { return AppDomain.GetCurrentThreadId(); }
+        static int s_mainthread_tid = get_thread_id();
+
+        static readonly object _locker = new object();
+        static DateTime s_last_DateTime = new DateTime(0);
+        //
+        static string text_elapse(int millisec)
+        {
+            if (millisec >= 1000)
+            {
+                double sec = (double)millisec / 1000;
+                return $"(+{sec:g}s)";
+            }
+            else if (millisec > 0) return $"(+{millisec}ms)";
+            else return "";
+        }
+        //
+        private void log(string s)
+        {
+//          lock (_locker)
+            {
+                DateTime nowdt = DateTime.Now;
+                int millisec_gap = (int)(nowdt - s_last_DateTime).TotalMilliseconds;
+                int seconds_gap = millisec_gap / 1000;
+                if (s_last_DateTime.Ticks > 0 && seconds_gap > 0)
+                {
+                    string text = "".PadLeft(Math.Min(10, seconds_gap), '.');
+                    PrintLine(text);
+                }
+
+                string tsprefix = string.Format("[{0:D2}:{1:D2}:{2:D2}.{3:D3}{4,-10}] ",
+                    nowdt.Hour, nowdt.Minute, nowdt.Second, nowdt.Millisecond, text_elapse(millisec_gap)
+                );
+                s_last_DateTime = nowdt;
+
+                PrintLine($"{tsprefix}{s}");
+            }
+        }
+
+        static string tid()
+        {
+            int tidnow = AppDomain.GetCurrentThreadId();
+            if (tidnow == s_mainthread_tid)
+                return "<tid=main>";
+            else
+                return $"<tid={tidnow}>";
+        }
+
+        void logtid(string s)
+        {
+            log(string.Format("{0,-11} {1}", tid(), s));
+        }
+
+        void PrintLine(string s)
+        {
+            Debug.WriteLine(s);
+            textBox1.Invoke(new Action(() =>
+            {
+                textBox1.AppendText(s + Environment.NewLine);
+            }));
+        }
+
+        void assert_main_thread() { Debug.Assert(get_thread_id() == s_mainthread_tid); }
+        void assert_not_main_thread() { Debug.Assert(get_thread_id() != s_mainthread_tid); }
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            long mem_used = GC.GetTotalMemory(true);
+            logtid($"Program start. GC.GetTotalMemory()={mem_used}");
+        }
+
+        private void btnStartNoCancel_Click(object sender, EventArgs e)
+        {
+            TestOnce(false);
+        }
+
+        private void btnStartWithCancel_Click(object sender, EventArgs e)
+        {
+            TestOnce(true);
+        }
+
+        private int s_taskdelay_msec = 1000;
+        private int s_mainthread_wait_msec = 2000;
+        private int s_cycles = 123000;
+
+        Task makeDelayTask()
+        {
+            Task tsk = Task.Delay(s_taskdelay_msec);
+            return tsk;
+        }
+
+        Task makeDelayTask_and_Cancel()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Task tsk = Task.Delay(s_taskdelay_msec, cts.Token);
+
+            cts.Cancel();
+            return tsk;
+        }
+
+        void CrazyTask(bool do_cancel)
+        {
+            for (int i = 0; i < s_cycles; i++)
+            {
+                if(!do_cancel)
+                {
+                    makeDelayTask();
+                }
+                else
+                {
+                    makeDelayTask_and_Cancel();
+                }
+            }
+            return;
+        }
+
+        void TestOnce(bool do_cancel)
+        {
+            btnStartNoCancel.Enabled = false;
+            btnStartWithCancel.Enabled = false;
+
+            char mark = do_cancel ? '-' : '*';
+
+            long m1 = GC.GetTotalMemory(true);
+            logtid($"{mark}MEM1: {m1}");
+
+            CrazyTask(do_cancel);
+
+            long m2 = GC.GetTotalMemory(true);
+            logtid($"{mark}MEM2: {m2}");
+
+            Thread.Sleep(s_mainthread_wait_msec); // 2000
+
+            long m3 = GC.GetTotalMemory(true);
+            logtid($"{mark}MEM3: {m3}");
+
+            btnStartNoCancel.Enabled = true;
+            btnStartWithCancel.Enabled = true;
+        }
+
+    }
+}
