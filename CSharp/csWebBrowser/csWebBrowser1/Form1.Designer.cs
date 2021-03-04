cdf6cfa1b9b5e30e1cfc5a086d764a5231e24acb
JimmChen vbwin7 <chjfth@gmail.com>
2021-03-04 11:06:05 +0800

    layout ok 1

--- CSharp/csWebBrowser/csWebBrowser1/Form1.Designer.cs
+++ CSharp/csWebBrowser/csWebBrowser1/Form1.Designer.cs
@@ -31,17 +31,18 @@ namespace prjSkeleton
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3Right = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tboxLog = new System.Windows.Forms.TextBox();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.cbxURL = new System.Windows.Forms.ComboBox();
             this.panel2Left = new System.Windows.Forms.Panel();
             this.btn1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.panel1.SuspendLayout();
             this.panel3Right.SuspendLayout();
-            this.panel2Left.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.panel2Left.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -57,28 +58,66 @@ namespace prjSkeleton
             // panel3Right
             // 
             this.panel3Right.Controls.Add(this.splitContainer1);
-            this.panel3Right.Controls.Add(this.label1);
-            this.panel3Right.Controls.Add(this.comboBox1);
-            this.panel3Right.Controls.Add(this.tboxLog);
+            this.panel3Right.Controls.Add(this.lblURL);
+            this.panel3Right.Controls.Add(this.cbxURL);
             this.panel3Right.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3Right.Location = new System.Drawing.Point(140, 0);
             this.panel3Right.Name = "panel3Right";
             this.panel3Right.Size = new System.Drawing.Size(765, 557);
             this.panel3Right.TabIndex = 1;
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(3, 21);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.Info;
+            this.splitContainer1.Panel2.Controls.Add(this.tboxLog);
+            this.splitContainer1.Size = new System.Drawing.Size(762, 530);
+            this.splitContainer1.SplitterDistance = 406;
+            this.splitContainer1.TabIndex = 3;
+            // 
             // tboxLog
             // 
-            this.tboxLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.tboxLog.BackColor = System.Drawing.SystemColors.Window;
+            this.tboxLog.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tboxLog.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tboxLog.Location = new System.Drawing.Point(0, 389);
+            this.tboxLog.Location = new System.Drawing.Point(0, 0);
             this.tboxLog.Multiline = true;
             this.tboxLog.Name = "tboxLog";
             this.tboxLog.ReadOnly = true;
             this.tboxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tboxLog.Size = new System.Drawing.Size(762, 168);
+            this.tboxLog.Size = new System.Drawing.Size(762, 120);
             this.tboxLog.TabIndex = 0;
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(3, 3);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(32, 13);
+            this.lblURL.TabIndex = 2;
+            this.lblURL.Text = "URL:";
+            // 
+            // cbxURL
+            // 
+            this.cbxURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxURL.FormattingEnabled = true;
+            this.cbxURL.Location = new System.Drawing.Point(52, 0);
+            this.cbxURL.Name = "cbxURL";
+            this.cbxURL.Size = new System.Drawing.Size(713, 21);
+            this.cbxURL.TabIndex = 1;
             // 
             // panel2Left
             // 
@@ -99,32 +138,6 @@ namespace prjSkeleton
             this.btn1.UseVisualStyleBackColor = true;
             this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(52, 0);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(710, 21);
-            this.comboBox1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "URL";
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Location = new System.Drawing.Point(92, 99);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            this.splitContainer1.Size = new System.Drawing.Size(520, 241);
-            this.splitContainer1.SplitterDistance = 74;
-            this.splitContainer1.TabIndex = 3;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -137,9 +150,11 @@ namespace prjSkeleton
             this.panel1.ResumeLayout(false);
             this.panel3Right.ResumeLayout(false);
             this.panel3Right.PerformLayout();
-            this.panel2Left.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.panel2Left.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -151,8 +166,8 @@ namespace prjSkeleton
         private System.Windows.Forms.TextBox tboxLog;
         private System.Windows.Forms.Panel panel2Left;
         private System.Windows.Forms.Button btn1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxURL;
+        private System.Windows.Forms.Label lblURL;
         private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
