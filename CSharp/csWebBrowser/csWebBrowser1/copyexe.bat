0c6dd19d67fa176602ec8423814e590ab6c37fc3
JimmChen vbwin7 <chjfth@gmail.com>
2021-03-05 20:40:26 +0800

    Add FEATURE_BROWSER_EMULATION registry file to ease experiment on new PC.

---
+++ CSharp/csWebBrowser/csWebBrowser1/copyexe.bat
@@ -0,0 +1,9 @@
+copy csWebBrowser1.exe wbIE7.exe
+copy csWebBrowser1.exe wbIE8.exe
+copy csWebBrowser1.exe wbIE8888.exe
+copy csWebBrowser1.exe wbIE9.exe
+copy csWebBrowser1.exe wbIE9999.exe
+copy csWebBrowser1.exe wbIE10.exe
+copy csWebBrowser1.exe wbIE10001.exe
+copy csWebBrowser1.exe wbIE11.exe
+copy csWebBrowser1.exe wbIE11001.exe
