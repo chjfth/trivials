d65d0a86815021d1f22a3dfa087119ec946b11ce
chj Win7Evernote VM <chjfth@gmail.com>
2021-10-07 13:01:09 +0800

    Add more cases.

--- mswin/seeRetBuf/seeRetBuf.cpp
+++ mswin/seeRetBuf/seeRetBuf.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <Psapi.h>
 #include <Userenv.h>
+#include <locale.h>
 #include <assert.h>
 #include <tchar.h>
 
@@ -13,7 +14,7 @@ DWORD winerr = 0;
 TCHAR soutput[MAX_PATH];
 TCHAR eoutput[MAX_PATH];
 
-#define SMALL_BUFCHARS 6
+#define SMALL_BUFCHARS 5
 
 #ifdef UNICODE
 #define Memset wmemset
@@ -37,6 +38,28 @@ void see_GetKeyNameText()
 	winerr = GetLastError();
 }
 
+void see_GetClassName()
+{
+	RESET_OUTPUT;
+	HWND hwnd = GetDesktopWindow();
+	sret = GetClassName(hwnd, soutput, MAX_PATH); // BufCHARS-1
+	// -- #32769
+	eret = GetClassName(hwnd, eoutput, SMALL_BUFCHARS); // T
+	winerr = GetLastError();
+}
+
+void see_GetTextFace()
+{
+	RESET_OUTPUT;
+	HDC hdc = GetDC(NULL);
+	sret = GetTextFace(hdc, MAX_PATH, soutput); // BufCHARS
+	// -- System
+	eret = GetTextFace(hdc, SMALL_BUFCHARS, eoutput); // T+1
+	winerr = GetLastError();
+
+	ReleaseDC(NULL, hdc);
+}
+
 void see_GetModuleFileName()
 {
 	RESET_OUTPUT;
@@ -45,6 +68,16 @@ void see_GetModuleFileName()
 	eret = GetModuleFileName(NULL, eoutput, SMALL_BUFCHARS);
 	winerr = GetLastError();
 }
+
+void see_GetModuleFileName_0buf()
+{
+	RESET_OUTPUT;
+	sret = GetModuleFileName(NULL, soutput, MAX_PATH);
+	// -- D:\gitw\trivials\mswin\seeRetBuf\Debug\seeRetBuf.exe
+	eret = GetModuleFileName(NULL, NULL, 0);
+	winerr = GetLastError();
+}
+
 
 void see_GetProcessImageFileName()
 {
@@ -104,13 +137,13 @@ void see_GetDefaultUserProfileDirectory()
 	winerr = GetLastError();
 }
 
-void see_GetLocaleInfo_smallbuf()
+void see_GetLocaleInfo()
 {
 	RESET_OUTPUT;
 	LCID lcid = LOCALE_USER_DEFAULT;
 	LCTYPE lctype = LOCALE_SLONGDATE;
 	sret = GetLocaleInfo(lcid, lctype, soutput, MAX_PATH);
-	// -- 
+	// -- MMMM d, yyyy
 	eret = GetLocaleInfo(lcid, lctype, eoutput, SMALL_BUFCHARS); // 0
 	winerr = GetLastError();
 }
@@ -121,17 +154,55 @@ void see_GetLocaleInfo_0buf()
 	LCID lcid = LOCALE_USER_DEFAULT;
 	LCTYPE lctype = LOCALE_SLONGDATE;
 	sret = GetLocaleInfo(lcid, lctype, soutput, MAX_PATH);
-	// -- 
+	// -- MMMM d, yyyy
+	_tprintf(_T("LOCALE_SLONGDATE: %s\n"), soutput);
 	eret = GetLocaleInfo(lcid, lctype, eoutput, 0); // ReqCHARS+1
+	winerr = GetLastError();
+}
+
+void see_FormatMessage()
+{
+	RESET_OUTPUT;
+	DWORD sampleerr = ERROR_FILE_NOT_FOUND;
+	sret = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, 
+		sampleerr, 
+		0, // LANGID
+		soutput, MAX_PATH,
+		NULL);
+	// -- The system cannot find the file specified.
+	eret = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, 
+		sampleerr, 
+		0, // LANGID
+		eoutput, SMALL_BUFCHARS,
+		NULL);
+	winerr = GetLastError();
+}
+
+#include <InitGuid.h>
+DEFINE_GUID(myguid,
+	0x20161110, 0x5434, 0x11d3, 0xb8, 0x90, 0x0, 0xc0, 0x4f, 0xad, 0x51, 0x71);
+
+void see_StringFromGUID2()
+{
+	RESET_OUTPUT;
+	sret = StringFromGUID2(myguid, soutput, MAX_PATH);
+	// -- {20161110-5434-11D3-B890-00C04FAD5171}
+	eret = StringFromGUID2(myguid, eoutput, SMALL_BUFCHARS);
 	winerr = GetLastError();
 }
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	setlocale(LC_ALL, ""); // only for printf Chinese chars purpose
+
 	see_GetKeyNameText();
+	see_GetClassName();
+
+	see_GetTextFace();
 	
 	see_GetModuleFileName();
+	see_GetModuleFileName_0buf();
 	
 	see_GetProcessImageFileName();
 	
@@ -143,9 +214,12 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	see_GetDefaultUserProfileDirectory();
 
-	see_GetLocaleInfo_smallbuf();
+	see_GetLocaleInfo();
 	see_GetLocaleInfo_0buf();
 
+	see_FormatMessage();
+
+	see_StringFromGUID2();
 
 	return 0;
 }
