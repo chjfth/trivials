88b287a74e7cf33f38134528994d38f65ffc0bcd
Jimm chja20 <chjfth@gmail.com>
2022-06-02 23:24:11 +0800

    VS2019 CMake project demo

---
+++ VSIDE/CMakeCons1/CMakeCons1/CMakeCons1.cpp
@@ -0,0 +1,21 @@
+﻿// CMakeCons1.cpp : Defines the entry point for the application.
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include "CMakeCons1.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello CMake." << endl;
+
+	const char *home = getenv("HOME"); // Linux
+	const char *userprofile = getenv("USERPROFILE"); // Windows
+	const char *myval = getenv("MYVAL");
+
+	printf("HOME        = %s\n", home);
+	printf("USERPROFILE = %s\n", userprofile);
+	printf("MYVAL=%s\n", myval);
+
+	return 0;
+}
