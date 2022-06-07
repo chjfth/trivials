6dcf13b575b243c7dc3586e66527dca87b1b5d6b
Jimm Win10 VM <chjfth@gmail.com>
2022-06-07 15:28:53 +0800

    Demonstrate $project/.vs/launch.vs.json's various launch configurations

---
+++ VSIDE/CMakeConse/CMakeConse/CMakeConse.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeConse.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeConse.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "argc = " << argc << endl;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
