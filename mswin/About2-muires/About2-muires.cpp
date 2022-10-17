ce72b251fceff248d97232704bcae7d4d1643486
Jimm Chen WDK10 <chjfth@gmail.com>
2022-10-17 17:01:58 +0800

    Simplify About2-muires

--- mswin/About2-muires/About2-muires.cpp
+++ mswin/About2-muires/About2-muires.cpp
@@ -18,44 +18,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	PSTR szCmdLine, int iCmdShow)
 {
 	static TCHAR szAppName[] = TEXT("About2-muires");
-	MSG          msg;
-	HWND         hwnd;
-	WNDCLASS     wndclass;
 
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = 0;
-	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIcon(hInstance, szAppName);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndclass.lpszMenuName = szAppName;
-	wndclass.lpszClassName = szAppName;
+	DialogBox(hInstance, TEXT("AboutBox"), NULL, AboutDlgProc);
 
-	if (!RegisterClass(&wndclass))
-	{
-		MessageBox(NULL, TEXT("This program requires Windows NT!"),
-			szAppName, MB_ICONERROR);
-		return 0;
-	}
-
-	hwnd = CreateWindow(szAppName, TEXT("About2 - with MUI .rc resource"),
-		WS_OVERLAPPEDWINDOW,
-		400, 200, // CW_USEDEFAULT, CW_USEDEFAULT,
-		400, 200, // CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL, NULL, hInstance, NULL);
-
-	ShowWindow(hwnd, iCmdShow);
-	UpdateWindow(hwnd);
-
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	return (int)msg.wParam;
+	return 0;
 }
+
 
 void PaintWindow(HWND hwnd, int iColor, int iFigure)
 {
@@ -89,44 +57,6 @@ void PaintTheBlock(HWND hCtrl, int iColor, int iFigure)
 	InvalidateRect(hCtrl, NULL, TRUE);
 	UpdateWindow(hCtrl);
 	PaintWindow(hCtrl, iColor, iFigure);
-}
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	static HINSTANCE hInstance;
-	PAINTSTRUCT      ps;
-
-	switch (message)
-	{
-	case WM_CREATE:
-		hInstance = ((LPCREATESTRUCT)lParam)->hInstance;
-
-		// Chj: Let the color selection dialog pops out automatically.
-		PostMessage(hwnd, WM_COMMAND, GET_WM_COMMAND_MPS(IDM_APP_ABOUT, 0, 0));
-		return 0;
-
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case IDM_APP_ABOUT:
-			if (DialogBox(hInstance, TEXT("AboutBox"), hwnd, AboutDlgProc))
-				InvalidateRect(hwnd, NULL, TRUE);
-			return 0;
-		}
-		break;
-
-	case WM_PAINT:
-		BeginPaint(hwnd, &ps);
-		EndPaint(hwnd, &ps);
-
-		PaintWindow(hwnd, iCurrentColor, iCurrentFigure);
-		return 0;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
-	return DefWindowProc(hwnd, message, wParam, lParam);
 }
 
 INT_PTR CALLBACK AboutDlgProc(HWND hDlg, UINT message,
