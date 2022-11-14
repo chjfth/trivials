75516042aec0fd99b66d281b72a24528072d5f1e
chj Win7Evernote VM <chjfth@gmail.com>
2022-11-14 11:54:15 +0800

    subexe2 : See behavior of CreateProcess.

---
+++ mswin/subexe2/subexe2.cpp
@@ -0,0 +1,161 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <locale.h>
+
+typedef DWORD WinErr_t;
+
+WinErr_t myCreateProcess(
+	const TCHAR *pszExepath,
+	TCHAR *szCommandLine, bool isShowWindow, bool isWait)
+{
+	STARTUPINFO sti = {sizeof(STARTUPINFO)};
+	sti.dwFlags = STARTF_USESHOWWINDOW;
+	sti.wShowWindow = isShowWindow ? SW_SHOW : SW_HIDE;
+
+	PROCESS_INFORMATION procinfo = {0};
+
+	BOOL succ = CreateProcess(
+		pszExepath,
+		szCommandLine, 
+		NULL, // process security attr
+		NULL, // thread security attr
+		FALSE, // bInheritHandles 
+		isShowWindow ? 0 : CREATE_NO_WINDOW, // dwCreationFlags
+		NULL, // lpEnvironment
+		NULL, // lpCurrentDirectory  
+		&sti, 
+		&procinfo);
+	if(!succ)
+		return GetLastError();
+
+	WinErr_t ret = ERROR_SUCCESS;
+
+	if(isWait)
+	{
+		BOOL waitre = WaitForSingleObject(procinfo.hProcess, INFINITE);
+		if(waitre==WAIT_OBJECT_0)
+		{
+			if(GetExitCodeProcess(procinfo.hProcess, &ret)==FALSE)
+				ret = GetLastError();
+		}
+		else
+			ret = GetLastError();
+	}
+
+	CloseHandle(procinfo.hThread);
+	CloseHandle(procinfo.hProcess);
+
+	return ret;
+}
+
+const TCHAR *wincmdline_strip_argv0(
+	const TCHAR *cmdline, const TCHAR *argv0, int *p_nspaces_after_argv0=0)
+{
+	// argv0 is definitely NOT surround by double-quotes.
+	// cmdline may or may not be surround by double-quotes.
+	//
+	// Now checking cmdline, there may be some space-chars 
+	// after argv0 part or after "argv0" part, we count the space-chars
+
+	const TCHAR * argv0_in_cmdline = _tcsstr(cmdline, argv0);
+	if(!argv0_in_cmdline)
+	{
+		_tprintf(_T("Unexpect! Cannot find argv[0] in cmdline.\n"));
+		exit(4);
+	}
+
+	const TCHAR *pv0tail = argv0_in_cmdline + _tcslen(argv0);
+
+	if(pv0tail[0]=='"')
+	{
+		pv0tail++;
+	}
+
+	if(pv0tail[0]=='\0')
+	{
+		if(p_nspaces_after_argv0)
+			*p_nspaces_after_argv0 = 0;
+
+		return nullptr;
+	}
+
+	// Now count space-chars
+	int nspc = 0;
+	while( pv0tail[nspc] == ' ' )
+		nspc++;
+
+	if(nspc==0)
+	{
+		_tprintf(_T("Unexpect! No space delimiter found after argv[0] part.\n"));
+		exit(4);		
+	}
+
+	if(p_nspaces_after_argv0)
+		*p_nspaces_after_argv0 = nspc;
+
+	if(pv0tail[nspc]!='\0')
+		return pv0tail + nspc;
+	else
+		return nullptr;
+}
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	// Will use env-var SUBEXE's value as CreateProcess's first param(lpApplicationName).
+
+	TCHAR exepath[MAX_PATH]={};
+	GetEnvironmentVariable(_T("SUBEXE"), exepath, MAX_PATH-1);
+	if(exepath[0])
+	{
+		_tprintf(_T("Will call CreateProcess() with first param from env-var SUBEXE:\n"));
+		_tprintf(_T("    %s\n"), exepath);
+	}
+	else
+	{
+		_tprintf(_T("No env-var SUBEXE found. Will call CreateProcess() with first param=NULL.\n"));
+	}
+
+	// Will use the whole string after argv[0] as CreateProcess's second param(lpCommandLine).
+	// If empty content after argv[0], NULL is used as CreateProcess's second param.
+	//
+	// If you want to pass pns to lpCommandLine with pns[0]='\0', you should write:
+	//
+	//	  subexe2 ""
+
+	const TCHAR *subexe_cmdline = wincmdline_strip_argv0(GetCommandLine(), argv[0]);
+	TCHAR szCmdLine[MAX_PATH] = {};
+
+	if(subexe_cmdline)
+	{
+		_tprintf(_T("Will call CreateProcess() with second param: (%d chars)\n"), _tcslen(subexe_cmdline));
+		_tprintf(_T("    %s\n"), subexe_cmdline);
+		_tcscpy_s(szCmdLine, subexe_cmdline);
+	}
+	else
+	{
+		_tprintf(_T("Will call CreateProcess() with second param=NULL.\n"));
+	}
+
+	WinErr_t winerr = myCreateProcess(
+		exepath[0] ? exepath : NULL,
+		szCmdLine, 
+		true, 
+		true // is wait
+		); // MSDN says: szCmdLine[] may be modified inside; he adds NUL after argv[0].
+
+	if(winerr==0)
+		_tprintf(_T("CreateProcess() sucess.\n"), winerr);
+	else
+		_tprintf(_T("CreateProcess() fail. WinErr=%d\n"), winerr);
+
+	if(winerr==0)
+	{
+		_tprintf(_T("Check post-CreateProcess() command-line string: (%d chars)\n"),
+			_tcslen(szCmdLine));
+		_tprintf(_T("    %s\n"), szCmdLine);
+	}
+
+	return 0;
+}
