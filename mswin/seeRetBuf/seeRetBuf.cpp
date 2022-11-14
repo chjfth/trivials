1fbbc1b3d4ab4c1a0bb50b17599d500cf7064ffe
chj Win7Evernote VM <chjfth@gmail.com>
2022-11-15 00:04:23 +0800

    Add "sort" parameter to count how many behaviors there are. 12 are observed from current cases.
    > D:\gitw\trivials\mswin\seeRetBuf\bin-v100\Win32\Debug\seeRetBuf.exe sort
    API#,Behavior#,WinAPI,BufSmallRet,WinErr,BufSmallFill,GoodRet
    1,1,_snprintf_s,-1,0,Yes*,T
    2,2,GetCurrentDirectory,Total+1,0,No*,T
    3,2,GetSystemDirectory,Total+1,0,No*,T
    4,3,GetLocaleInfo(0buf),Total+1,0,No*,T+1
    5,4,GetEnvironmentVariable,Total+1,0,No+NUL,T
    6,4,GetTempPath,Total+1,0,No+NUL,T
    7,5,GetDefaultUserProfileDirectory,Total+1,122,Yes*,T+1
    8,6,GetTextFace,Usersize,0,Yes+NUL,T+1
    9,7,GetModuleFileName,Usersize,122,Yes+NUL,T
    10,8,GetClassName,Usersize-1,0,Yes+NUL,T
    11,8,GetKeyNameText,Usersize-1,0,Yes+NUL,T
    12,9,StringFromGUID2,Zero,0,No*,T+1
    13,10,GetModuleFileName(0buf),Zero,122,No*,T
    14,10,GetProcessImageFileName,Zero,122,No*,T
    15,11,GetLocaleInfo,Zero,122,No*,T+1
    16,12,FormatMessage,Zero,122,Yes+NUL,T

--- mswin/seeRetBuf/seeRetBuf.cpp
+++ mswin/seeRetBuf/seeRetBuf.cpp
@@ -43,7 +43,23 @@ TCHAR eoutput[MAX_PATH];
 } while(0)
 
 
-enum BufSmallRet_et // Ret trait when buffer small
+enum 
+{ 
+	MaxAPI = 100,
+	Traits_STRMAXLEN = 60,
+};
+
+struct ApiCase_st
+{
+	const TCHAR *apiname;
+	TCHAR traits[Traits_STRMAXLEN];  // e.g: "Total+1,0,No*,T"
+};
+
+ApiCase_st gar_apicases[MaxAPI];
+int g_nowcase = 0; // How many elements in gar_apicases[MaxAPI]
+
+
+enum BufSmallRet_et // Ret trait when buffer is too small
 {
 	BSR_Neg1 = -1,
 	BSR_Zero = 0,
@@ -57,10 +73,10 @@ enum BufSmallRet_et // Ret trait when buffer small
 
 enum BufSmallFill_et // User buffer filling trait when buffer small
 {
-	BSF_Pure_NUL = 0,
-	BSF_LeaveAsIs,
-	BSF_PartialAndNUL,
-	BSF_PartialNoNUL,
+	BSF_Pure_NUL = 0,  // fill NUL to userbuf[0]
+	BSF_LeaveAsIs,     // userbuf[] is not touched at all
+	BSF_PartialAndNUL, // userbuf[] is filled to its up-limit and final cell NUL
+	BSF_PartialNoNUL,  // userbuf[] is filled to its up-limit and final cell is not NUL
 
 	BSF_BUG
 };
@@ -68,10 +84,10 @@ enum BufSmallFill_et // User buffer filling trait when buffer small
 enum BufEnoughRet_et // Ret trait when buffer enough
 {
 	ReturnT = 0,      // Return exactly "total" size, the correct result's strlen().
-	                  // "Total"(T) does not count trailing NUL 
+	                  // "Total"(T) does not count trailing NUL. 
 	ReturnTplus1 = 1, // Return T+1
 
-	ReturnBug
+	ReturnBug = 2,
 };
 
 struct ApiTrait_et
@@ -155,6 +171,21 @@ BufEnoughRet_et BufEnoughRet_conclude(
 	return ReturnBug;
 }
 
+template <size_t bufchars>
+void vacat(
+	TCHAR (&userbuf)[bufchars],
+	const TCHAR *fmt, ...)
+{
+	int startlen = _tcslen(userbuf);
+
+	va_list args;
+	va_start(args, fmt);
+	
+	_vsntprintf_s(userbuf+startlen, bufchars-startlen, _TRUNCATE, fmt, args);
+
+	va_end(args);
+}
+
 void Prn(const TCHAR *fmt, ...)
 {
 	va_list args;
@@ -165,13 +196,29 @@ void Prn(const TCHAR *fmt, ...)
 	va_end(args);
 }
 
+void Add1Case(const TCHAR *apiname, const TCHAR *traits)
+{
+	int n = g_nowcase;
+	
+	if(n==MaxAPI)
+	{
+		_tprintf(_T("Error! Reached MaxAPI.\n"));
+		exit(4);
+	}
+
+	gar_apicases[n].apiname = apiname;
+	_tcscpy_s(gar_apicases[n].traits, traits);
+
+	g_nowcase++;
+}
+
 void ReportTraits(const TCHAR *apiname,
 	int user_size, int eret_size, DWORD winerr,
 	const TCHAR *eoutput, 
 	int sret_size, const TCHAR *soutput)
 
 {
-	Prn(_T("%s,"), apiname);
+	TCHAR tbuf[Traits_STRMAXLEN] = _T("");
 
 	ApiTrait_et t = {};
 	
@@ -179,57 +226,57 @@ void ReportTraits(const TCHAR *apiname,
 	switch(t.bs_ret)
 	{
 	case BSR_Neg1: 
-		Prn(_T("-1")); break;
+		vacat(tbuf, _T("-1")); break;
 	case BSR_Zero: 
-		Prn(_T("Zero")); break;
+		vacat(tbuf, _T("Zero")); break;
 	case BSR_Usersize: 
-		Prn(_T("Usersize")); break;
+		vacat(tbuf, _T("Usersize")); break;
 	case BSR_UsersizeLess1:
-		Prn(_T("Usersize-1")); break;
+		vacat(tbuf, _T("Usersize-1")); break;
 	case BSR_Total:
-		Prn(_T("Total")); break;
+		vacat(tbuf, _T("Total")); break;
 	case BSR_TotalPlus1:
-		Prn(_T("Total+1")); break;
+		vacat(tbuf, _T("Total+1")); break;
 	default:
-		Prn(_T("[BUG]"));
+		vacat(tbuf, _T("[BUG]"));
 	}
 	
-	Prn(_T(","));
+	vacat(tbuf, _T(","));
 
 	t.bs_winerr = winerr;
-	Prn(_T("%d"), t.bs_winerr);
+	vacat(tbuf, _T("%d"), t.bs_winerr);
 
-	Prn(_T(","));
+	vacat(tbuf, _T(","));
 
 	t.bs_fill = BufSmallFill_conclude(user_size, eret_size, eoutput, soutput);
 	switch(t.bs_fill)
 	{
 	case BSF_Pure_NUL:
-		Prn(_T("No+NUL")); break;
+		vacat(tbuf, _T("No+NUL")); break;
 	case BSF_LeaveAsIs:
-		Prn(_T("No*")); break;
+		vacat(tbuf, _T("No*")); break;
 	case BSF_PartialAndNUL:
-		Prn(_T("Yes+NUL")); break;
+		vacat(tbuf, _T("Yes+NUL")); break;
 	case BSF_PartialNoNUL:
-		Prn(_T("Yes*")); break;
+		vacat(tbuf, _T("Yes*")); break;
 	default:
-		Prn(_T("[BUG]"));
+		vacat(tbuf, _T("[BUG]"));
 	}
 	
-	Prn(_T(","));
+	vacat(tbuf, _T(","));
 
 	t.good_ret = BufEnoughRet_conclude(sret_size, soutput);
 	switch(t.good_ret)
 	{
 	case ReturnT:
-		Prn(_T("T")); break;
+		vacat(tbuf, _T("T")); break;
 	case ReturnTplus1:
-		Prn(_T("T+1")); break;
+		vacat(tbuf, _T("T+1")); break;
 	default:
-		Prn(_T("[BUG]"));
+		vacat(tbuf, _T("[BUG]"));
 	}
 
-	Prn(_T("\n"));
+	Add1Case(apiname, tbuf);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -432,25 +479,20 @@ void see_StringFromGUID2()
 	REPORT_API_TRAITS(StringFromGUID2);
 }
 
-
-int _tmain(int argc, _TCHAR* argv[])
+void check_apis()
 {
-	setlocale(LC_ALL, ""); // only for printf Chinese chars purpose
-
-	Prn(_T("WinAPI,BufSmallRet,WinErr,BufSmallFill,GoodRet\n"));
-
 	see_snprintf();
 
 	see_GetKeyNameText();
 	see_GetClassName();
 
 	see_GetTextFace();
-	
+
 	see_GetModuleFileName();
 	see_GetModuleFileName_0buf();
-	
+
 	see_GetProcessImageFileName();
-	
+
 	see_GetSystemDirectory();
 	see_GetCurrentDirectory();
 
@@ -465,6 +507,63 @@ int _tmain(int argc, _TCHAR* argv[])
 	see_FormatMessage();
 
 	see_StringFromGUID2();
+}
+
+int __cdecl CompareTraits(void *ctx, const void *t1, const void *t2)
+{
+	ApiCase_st &c1 = *(ApiCase_st*)t1;
+	ApiCase_st &c2 = *(ApiCase_st*)t2;
+	return _tcscmp(c1.traits, c2.traits);
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	setlocale(LC_ALL, ""); // only for printf Chinese chars purpose
+
+	bool is_sort = false;
+
+	if(argc>1 && _tcscmp(argv[1], _T("sort"))==0)
+	{
+		is_sort = true;
+	}
+
+	check_apis();
+
+	if(!is_sort)
+	{
+		Prn(_T("#,WinAPI,BufSmallRet,WinErr,BufSmallFill,GoodRet\n"));
+
+		int i;
+		for(i=0; i<g_nowcase; i++)
+		{
+			Prn(_T("%d,%s,%s\n"), i+1, gar_apicases[i].apiname, gar_apicases[i].traits);
+		}
+
+		Prn(_T("\n"));
+		Prn(_T("Hint: Add parameter \"sort\" to count how many behaviors there are.\n"));
+	}
+	else
+	{
+		qsort_s(gar_apicases, g_nowcase, sizeof(ApiCase_st), CompareTraits, NULL);
+
+		Prn(_T("API#,Behavior#,WinAPI,BufSmallRet,WinErr,BufSmallFill,GoodRet\n"));
+
+		int behavior = 0, i;
+		const TCHAR *prev_traits = _T("");
+		for(i=0; i<g_nowcase; i++)
+		{
+			const ApiCase_st &curcase = gar_apicases[i];
+
+			if(_tcscmp(prev_traits, curcase.traits)!=0)
+			{
+				behavior++;
+				prev_traits = curcase.traits;
+			}
+
+			Prn(_T("%d,%d,%s,%s\n"), i+1, behavior, 
+				gar_apicases[i].apiname, gar_apicases[i].traits);
+		}
+	}
 
 	return 0;
 }
