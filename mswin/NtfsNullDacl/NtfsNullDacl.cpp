8cd9b48470f560536ce20de7d476df9e8f19b5a4
chj Win7Evernote VM <chjfth@gmail.com>
2022-11-22 23:36:57 +0800

    Add pedagogical code.

--- mswin/NtfsNullDacl/NtfsNullDacl.cpp
+++ mswin/NtfsNullDacl/NtfsNullDacl.cpp
@@ -8,11 +8,26 @@
 #include <tchar.h>
 #include <locale.h>
 
-void SetNullDacl(const TCHAR *szfn, bool is_protect)
+enum ProtectFlag_et
+{
+	DontMention = 0, 
+	WantProtectBit = 1,
+	WantUnprotectBit = 2,
+};
+
+void SetNullDacl(const TCHAR *szfn, ProtectFlag_et pf)
 {
 	SECURITY_INFORMATION siflags = DACL_SECURITY_INFORMATION;
+	const TCHAR *pfhint = _T("");
 
-	siflags |= (is_protect ? PROTECTED_DACL_SECURITY_INFORMATION : UNPROTECTED_DACL_SECURITY_INFORMATION);
+	if(pf==WantProtectBit) {
+		siflags |= PROTECTED_DACL_SECURITY_INFORMATION;
+		pfhint = _T("(+PROTECT)");
+	}
+	else if(pf==WantUnprotectBit) {
+		siflags |= UNPROTECTED_DACL_SECURITY_INFORMATION;
+		pfhint = _T("(-PROTECT)");
+	}
 
 	// [2022-11-22] On Win7, even if we add UNPROTECTED_DACL_SECURITY_INFORMATION flag,
 	// setting DACL_SECURITY_INFORMATION with pDacl=NULL will make Windows ignore that
@@ -26,9 +41,7 @@ void SetNullDacl(const TCHAR *szfn, bool is_protect)
 
 	if(winerr==0)
 	{
-		_tprintf(_T("SetNamedSecurityInfo(), set NULL-DACL %s success.\n"),
-			is_protect ? _T("(+PROTECT)" : _T(""))
-			);
+		_tprintf(_T("SetNamedSecurityInfo(), set NULL-DACL %s success.\n"), pfhint);
 	}
 	else
 	{
@@ -46,19 +59,28 @@ int _tmain(int argc, TCHAR* argv[])
 	if(argc==1)
 	{
 		const TCHAR *s = _T("D:\\test\\foo.txt");
-		_tprintf(_T("This program sets an NTFS file/directory to have NULL-DACL, so to be readable/writeable by EVERYONE.\n"));
+		_tprintf(_T("This program sets an NTFS file/directory to have NULL-DACL, so to be readable/writable/ownable by EVERYONE.\n"));
 		_tprintf(_T("Missing parameters.\n"));
 		_tprintf(_T("Example:\n"));
 		_tprintf(_T("    NtfsNullDacl %s\n"), s);
 		_tprintf(_T("Explicitly add PROTECTED_DACL_SECURITY_INFORMATION flag(exp, no actual effect):\n"));
-		_tprintf(_T("    NtfsNullDacl %s P\n"), s);
+		_tprintf(_T("    NtfsNullDacl %s +P\n"), s);
+		_tprintf(_T("Explicitly add UNPROTECTED_DACL_SECURITY_INFORMATION flag(exp, no actual effect):\n"));
+		_tprintf(_T("    NtfsNullDacl %s -P\n"), s);
 		exit(1);
 	}
 	const TCHAR *szfn = argv[1];
 
-	bool is_protect = (argc>2 && toupper(argv[2][0])=='P') ? true : false;
+	ProtectFlag_et pf = DontMention;
+	if(argc>2)
+	{
+		if(argv[2][0]=='+' && toupper(argv[2][1])=='P')
+			pf = WantProtectBit;
+		else if(argv[2][0]=='-' && toupper(argv[2][1])=='P')
+			pf = WantUnprotectBit;
+	}
 
-	SetNullDacl(szfn, is_protect);
+	SetNullDacl(szfn, pf);
 
 	return 0;
 }
