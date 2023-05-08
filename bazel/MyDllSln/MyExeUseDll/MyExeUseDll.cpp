21618d23ebf1fd0c3260b03a05df3d0a93f2a696
Jimm Chen WDK10 <chjfth@gmail.com>
2023-05-08 20:51:13 +0800

    Bazel : Linux use .so OK

--- bazel/MyDllSln/MyExeUseDll/MyExeUseDll.cpp
+++ bazel/MyDllSln/MyExeUseDll/MyExeUseDll.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <tchar.h>
+//#include <tchar.h>
 #include <locale.h>
 
 #include <MyDll.h>
 
-int _tmain(int argc, TCHAR* argv[])
+int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "");
 	
 	MyDll_version_st vst = {};
 	MyDll_getversion(&vst);
 
-	_tprintf(_T("EXE: Using MyDll version: %d.%d.%d.%d\n"), vst.major, vst.minor, vst.patch, vst.extra);
+	printf("EXE: Using MyDll version: %d.%d.%d.%d\n", vst.major, vst.minor, vst.patch, vst.extra);
 
 	return 0;
 }
