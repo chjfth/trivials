ef0f3595c21ce0a6c55136cd66240eb6c2dadb64
Jimm Chen WDK10 <chjfth@gmail.com>
2023-05-31 10:47:24 +0800

    fresh fix to prev commit, add summary comments.

--- cpp/throw_heapobj/throw_heapobj.cpp
+++ cpp/throw_heapobj/throw_heapobj.cpp
@@ -5,12 +5,12 @@ class CBall // the exception object to throw
 public:
 	CBall(const char *i_msg) : errmsg(i_msg)
 	{
-		printf("CBall [%p] created.\n");
+		printf("CBall ctor, [%p] created.\n", this);
 	}
 
 	~CBall()
 	{
-		printf("CBall [%p] deleted.\n");
+		printf("CBall dtor, [%p] deleted.\n", this);
 	}
 
 	CBall(const CBall &oldball)
@@ -78,6 +78,7 @@ int main(int argc, char *argv[])
 {
 	const char* ptrexe = argv[0];
 	printf("argv[0] = %s\n", ptrexe);
+	printf("Localvar at: [%p]\n", &ptrexe);
 	printf("\n");
 
 	try
@@ -86,11 +87,24 @@ int main(int argc, char *argv[])
 	}
 	catch (const CBall &ball)
 	{
-		printf("[CaughtIt] ball at: [%p]\n", &ball);
-		printf("           errmsg : %s\n", ball.errmsg);
+		printf("[CaughtIt] excpt-obj at: [%p]\n", &ball);
+		printf("           errmsg      : %s\n", ball.errmsg);
 
-		// Q: Has autoball deleted? or a new obj?
+		// Q: Has autoball(from sth_wrong1) deleted? or a new obj?
 	}
 
 	printf("main() final line.\n");
 }
+
+/*
+ * Key points of this toy-program:
+ *
+ * (1) In sth_wrong1(), it throws a heap object(an object created with `new`), instead of a temporary.
+ *     the result is: C++ engine makes a copy of the thrown object(using copy-ctor) to create a
+ *     "local object" at caught site.
+ *     
+ * (2) The lifetime of the original thrown object, obey the orginal rule when "no throw() is used".
+ *
+ * (3) At caught-site, Whether the excpt-obj contains [valid pointers to other members], is not cared
+ *     by C++ engine. The coder should be responsible for it by himself.
+ */
