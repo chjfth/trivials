5f8c24e302809eb284a1176f8d37b4861fca7916
Jimm Chen WDK10 <chjfth@gmail.com>
2023-06-08 21:35:24 +0800

    temp excptest2, a C++ object in doWork()

---
+++ cpp/202306-excptest/excptest2/excptest2.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdexcept>
+
+extern void will_throw(unsigned int);
+
+void doWork(unsigned int ui)
+{
+	char bufa[8] = "AAAAAAA";
+	will_throw(0xBBBBbbbb);
+}
+
+int main(int argc, char *argv[])
+{
+	bool path1 = (argc > 1 && argv[1][0] == '1') ? true : false;
+	
+	printf("Start try{} ...\n");
+	try
+	{
+		doWork(0xAAAAaaaa);
+	}
+	catch (std::exception &e)
+	{
+		printf("[Caught!] e.what() is: %s\n", e.what());
+	}
+	
+	printf("Safe return from will_throw().\n");
+	
+	return 0;
+}
+
+// VC2019 compile and link command
+//	cl /EHsc /MT /Zi /JMC- /GS- /FAsc excptest2.cpp worklib.cpp
