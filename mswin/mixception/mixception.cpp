0e545ee1167bc55b43a2b3ed4810e6a0a632f932
Jimm Chen WDK10 <chjfth@gmail.com>
2023-06-24 08:02:53 +0800

    mixception Console-Debug

---
+++ mswin/mixception/mixception.cpp
@@ -0,0 +1,29 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+
+int do_divide(int dividend, int divisor)
+{
+	int result = dividend / divisor;
+	return result;
+}
+
+int do_work()
+{
+	const TCHAR* pz = GetCommandLine();
+	int slen = _tcslen(pz);
+
+	int divisor = 0;
+	if (pz[slen - 1] == '1') // if command-line end with param "1"
+		divisor = 1;
+
+	int result = 6 / divisor;
+	return result;
+}
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	return do_work();
+}
