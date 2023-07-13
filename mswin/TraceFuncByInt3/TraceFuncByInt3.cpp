764d7c52682e9e70e09eaea3074290ae2d9aefd8
chj Win7Evernote VM <chjfth@gmail.com>
2023-07-13 11:24:30 +0800

    Add running sample output.

--- mswin/TraceFuncByInt3/TraceFuncByInt3.cpp
+++ mswin/TraceFuncByInt3/TraceFuncByInt3.cpp
@@ -22,9 +22,9 @@ void test_tracing_myfunc()
 {
 	Trace_UserFunc(myfunc1);
 
-	myfunc1(1);
+	myfunc1(2);
 	printf("\n");
-	Sleep(500);
+	Sleep(500); // no importance
 
 	printf("== Try some other exception ==\n");
 	int ret = 0;
@@ -36,7 +36,7 @@ void test_tracing_myfunc()
 	}
 	printf("\n");
 
-	myfunc1(2);
+	myfunc1(3);  // no importance
 	printf("\n");
 	Sleep(500);
 
@@ -45,9 +45,39 @@ void test_tracing_myfunc()
 
 int main()
 {
+	myfunc1(1);
+	printf("\n");
+
 	test_tracing_myfunc();
 
-	myfunc1(3);
+	myfunc1(4);
 
 	return 0;
 }
+
+/* [2023-07-13] Running output is sth like this:
+
+C:\>D:\gitw\trivials\mswin\TraceFuncByInt3\bin-v100\Win32\Debug\TraceFuncByInt3.exe
+myfunc1(1) really executing ...
+
+[Matt] In int3_BreakpointHandler: ExcpCode=0x80000003 EIP=00401020
+[Matt] Do STATUS_BREAKPOINT handling.
+[Matt] ## Monitored function is being called.
+[Matt] In int3_BreakpointHandler: ExcpCode=0x80000004 EIP=00401021
+[Matt] Do STATUS_SINGLE_STEP handling.
+myfunc1(2) really executing ...
+
+== Try some other exception ==
+[Matt] In int3_BreakpointHandler: ExcpCode=0xC0000094 EIP=0040100F
+[Caught] division by zero.
+
+[Matt] In int3_BreakpointHandler: ExcpCode=0x80000003 EIP=00401020
+[Matt] Do STATUS_BREAKPOINT handling.
+[Matt] ## Monitored function is being called.
+[Matt] In int3_BreakpointHandler: ExcpCode=0x80000004 EIP=00401021
+[Matt] Do STATUS_SINGLE_STEP handling.
+myfunc1(3) really executing ...
+
+myfunc1(4) really executing ...
+
+*/
