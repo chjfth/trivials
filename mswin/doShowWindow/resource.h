cdc4d00ba411cb5180c2fd3952c88567ef96430d
chj Win7Evernote VM <chjfth@gmail.com>
2024-11-07 08:27:46 +0800

    add missing file

---
+++ mswin/doShowWindow/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by doShowWindow.rc
+//
+#define IDI_WINMAIN                     100
+#define IDD_WINMAIN                     101
+#define IDC_EDIT_INFO                   1001
+#define IDC_BTN_EXECUTE                 1002
+#define IDC_EDIT_HWND                   1003
+#define IDC_LABEL2                      1004
+#define IDC_nCmdShow                    1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
