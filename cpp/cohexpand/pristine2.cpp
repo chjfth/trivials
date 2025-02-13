d52ecd25b8ecac957a68e3e64e0999c98b78c892
chj Win7Evernote VM <chjfth@gmail.com>
2025-02-13 16:28:07 +0800

    Cohexpand prstine2 demoo

---
+++ cpp/cohexpand/pristine2.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <wchar.h>
+
+// Compared to pristine1.cpp, this version applies WRAP_BQ()'s result to wide-string.
+
+#define cox_MAKE_STRING(asymbol) #asymbol
+#define coy_MAKE_STRING(s) cox_MAKE_STRING(s)
+
+#define WRAP_B(s) [s]
+#define WRAP_BQ(s) coy_MAKE_STRING( WRAP_B(s) )
+
+const char *s_greet = WRAP_BQ(greet);
+
+////
+
+#define cox_MAKE_WIDE(astring) L ## astring
+#define coy_MAKE_WIDE(s) cox_MAKE_WIDE(s)
+const wchar_t *sw_greet = coy_MAKE_WIDE( WRAP_BQ(greet) );
+
+
+int main(int argc, char* argv[])
+{
+	printf("[A]greet: %s\n", s_greet);
+
+#ifdef _WIN32
+	wprintf(L"[w]greet: %s\n", sw_greet); // MSVCRT: small %s for wide-string
+#else
+	// Reopen stdout in wide mode, Linux gcc requires this.
+	freopen(NULL, "w", stdout);
+	wprintf(L"[W]greet: %ls\n", sw_greet); // Glibc: %ls for wide-string
+#endif
+
+	return 0;
+}
