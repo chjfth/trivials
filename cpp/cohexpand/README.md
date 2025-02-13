3df8a4fa6e6c31a58b4e9c731b8ec2b5b93cfb8d
chj Win7Evernote VM <chjfth@gmail.com>
2025-02-13 16:17:51 +0800

    Cohexpand prstine1 demoo

---
+++ cpp/cohexpand/README.md
@@ -0,0 +1,3 @@
+﻿cohexpand [kəʊˈhespænd]: Cohesive Expand
+
+Explaining the behavior of C99 macro's # and ## operator. 
---
+++ cpp/cohexpand/pristine1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define cox_MAKE_STRING(asymbol) #asymbol
+#define coy_MAKE_STRING(s) cox_MAKE_STRING(s)
+
+#define WRAP_B(s) [s]
+#define WRAP_BQ(s) coy_MAKE_STRING( WRAP_B(s) )
+
+const char *s_greet = WRAP_BQ(greet);
+// -- We get a static string "[greet]".
+// The beauti-fact is: we can #define greet to be any string from compiler command-line.
+// For example:
+//		cl  /D greet="Hello world!" pristine1.cpp
+//		gcc -D greet='Hello world!' pristine1.cpp
+
+int main(int argc, char* argv[])
+{
+	printf("greet: %s\n", s_greet);
+	return 0;
+}
