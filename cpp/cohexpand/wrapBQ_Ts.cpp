9ac7e68f0ef0a580f4659b39770df53fa9bae5a0
chj Win7Evernote VM <chjfth@gmail.com>
2025-02-13 17:30:28 +0800

    update wrapBQ_Ts.cpp to wrap 1,2 and 3 pairs of space-chars.

--- cpp/cohexpand/wrapBQ_Ts.cpp
+++ cpp/cohexpand/wrapBQ_Ts.cpp
@@ -1,5 +1,5 @@
 // Compare to wrapBQ_T.cpp, this version wraps with extra starting & ending space-chars,
-// implemented as WRAP_BSQ() macro.
+// implemented as WRAP_BSQ1(), WRAP_BSQ2(), WRAP_BSQ3() macro.
 // Starting & ending Space-chars are defaultly stripped off from macro arguments,
 // so some special treatment is required. (still mysterious)
 
@@ -13,16 +13,27 @@
 #define NN
 #define WW NN NN
 
-#define WRAP_BS(s) NN [s] WW
+#define WRAP_BS1(s) NN [s] WW // note: the trailing must be WW, not NN
+#define WRAP_BS2(s) NN WRAP_BS1(s) NN
+#define WRAP_BS3(s) NN WRAP_BS2(s) NN
 
-#define WRAP_BSQ(s) coy_MAKE_STRING(WRAP_BS(s))
-// -- Now we get " [greet] ".
+#define WRAP_BSQ1(s) coy_MAKE_STRING(WRAP_BS1(s)) // We get " [greet] "
+
+#define WRAP_BSQ2(s) coy_MAKE_STRING(WRAP_BS2(s))  // We get "  [greet]  "
+
+#define WRAP_BSQ3(s) coy_MAKE_STRING(WRAP_BS3(s))  // We get "   [greet]   "
 
 int _tmain(int argc, TCHAR* argv[])
 {
 	setlocale(LC_ALL, "");
 
-	const TCHAR *psz = _T(WRAP_BSQ(greet));
-	_tprintf(_T("[T]Greeting: {%s}\n"), psz);
+	const TCHAR *ps1 = _T(WRAP_BSQ1(greet));
+	_tprintf(_T("[T1]Greeting: {%s}\n"), ps1);
+
+	const TCHAR *ps2 = _T(WRAP_BSQ2(greet));
+	_tprintf(_T("[T2]Greeting: {%s}\n"), ps2);
+
+	const TCHAR *ps3 = _T(WRAP_BSQ3(greet));
+	_tprintf(_T("[T3]Greeting: {%s}\n"), ps3);
 	return 0;
 }
