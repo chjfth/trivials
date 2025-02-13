8b2c74c29667e54d27ce40ee9f2e36645194d5e5
Jimm Chen WDK10 <chjfth@gmail.com>
2025-02-13 11:19:09 +0800

    Now can be compiled with MSVC and Linux-gcc.

--- cpp/cohexpand/wrapBQ.cpp
+++ cpp/cohexpand/wrapBQ.cpp
@@ -2,6 +2,8 @@
 #include <locale.h>
 #include <wchar.h>
 
+// This code can be compiled with MSVC and Linux-gcc.
+
 #define cox_MAKE_STRING(asymbol) #asymbol
 #define cox_MAKE_WSTRING(asymbol) L ## #asymbol
 
@@ -25,7 +27,14 @@ int main(int argc, char* argv[])
 	setlocale(LC_ALL, "");
 	
 	printf("[A]greet: %s\n", s_greet);
-	wprintf(L"[W]greet: %s\n", sw_greet);
+
+#ifdef _WIN32
+	wprintf(L"[w]greet: %s\n", sw_greet); // small %s for wide-string
+#else
+	// Reopen stdout in wide mode, Linux gcc requires this.
+	freopen(NULL, "w", stdout);
+	wprintf(L"[W]greet: %S\n", sw_greet); // big %s for wide-string
+#endif
 
 	return 0;
 }
