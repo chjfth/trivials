0e44f526dfd0e069f2a772b6a5dc2a703c3500a9
chj Win7Evernote VM <chjfth@gmail.com>
2025-03-04 20:35:25 +0800

    guiSetTimeZone init0

---
+++ mswin/guiSetTimeZone/iversion.h
@@ -0,0 +1,28 @@
+#ifndef __guiSetTimeZone_iversion_h_
+#define __guiSetTimeZone_iversion_h_
+
+#define guiSetTimeZone_VMAJOR 1
+#define guiSetTimeZone_VMINOR 0
+#define guiSetTimeZone_VPATCH 0
+#define guiSetTimeZone_VTAIL  1
+
+#define guiSetTimeZonestr__(n) #n
+#define guiSetTimeZonestr(n) guiSetTimeZonestr__(n)
+
+// The following 4 are used in .rc
+#define guiSetTimeZone_VMAJORs guiSetTimeZonestr(guiSetTimeZone_VMAJOR)
+#define guiSetTimeZone_VMINORs guiSetTimeZonestr(guiSetTimeZone_VMINOR)
+#define guiSetTimeZone_VPATCHs guiSetTimeZonestr(guiSetTimeZone_VPATCH)
+#define guiSetTimeZone_VTAILs  guiSetTimeZonestr(guiSetTimeZone_VTAIL)
+
+#define guiSetTimeZone_NAME "guiSetTimeZone"
+
+enum {
+	guiSetTimeZone_vmajor = guiSetTimeZone_VMAJOR,
+	guiSetTimeZone_vminor = guiSetTimeZone_VMINOR,
+	guiSetTimeZone_vpatch = guiSetTimeZone_VPATCH,
+	guiSetTimeZone_vtail = guiSetTimeZone_VTAIL,
+};
+
+
+#endif
