0cc0e4d887c77481a04bbc84441c41d3b4cd3d22
chj Win7Evernote VM <chjfth@gmail.com>
2025-05-07 20:39:12 +0800

    tWinCursor1 v1.0

---
+++ mswin/tWinCursor1/tWinCursor1.cpp
@@ -0,0 +1,240 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <windowsx.h>
+#include <tchar.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#define EXE_VERSION "1.0"
+
+#include "utils.h"
+
+#include <mswin/winuser.itc.h>
+using namespace itc;
+
+LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM) ;
+
+int WINAPI _tWinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
+					PTSTR szCmdLine, int nCmdShow)
+{
+	(void)hPrevInstance; (void)szCmdLine; 
+	static TCHAR szAppName[] = TEXT ("tWinCursor1") ;
+	HWND         hwnd = NULL;
+	MSG          msg = {};
+	WNDCLASS     wndclass = {};
+
+	wndclass.style         = CS_HREDRAW | CS_VREDRAW ;
+	wndclass.lpfnWndProc   = WndProc ;
+	wndclass.cbClsExtra    = 0 ;
+	wndclass.cbWndExtra    = 0 ;
+	wndclass.hInstance     = hInstance ;
+	wndclass.hIcon         = LoadIcon (hInstance, MAKEINTRESOURCE(1)) ;
+	wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW) ;
+	wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
+	wndclass.lpszMenuName  = NULL ;
+	wndclass.lpszClassName = szAppName ;
+
+	RegisterClass (&wndclass);
+	 
+	hwnd = CreateWindow (szAppName,    // window class name
+		TEXT ("The tWinCursor1 Program"), // window caption
+		WS_OVERLAPPEDWINDOW,           // window style
+		CW_USEDEFAULT,   // initial x position
+		CW_USEDEFAULT,   // initial y position
+		400,             // initial x size
+		200,             // initial y size
+		NULL,            // parent window handle
+		NULL,            // window menu handle
+		hInstance,       // program instance handle
+		NULL) ;          // creation parameters
+	 
+	ShowWindow (hwnd, nCmdShow) ;
+	UpdateWindow (hwnd) ;
+	
+	static int s_count = 0;
+
+	while (GetMessage (&msg, NULL, 0, 0))
+	{
+		itc::String wmstr = ITCSv(msg.message, WM_xxx);
+
+		vaDbgTs(_T("#%d - MSG: %s >>>" ), ++s_count, wmstr.get());
+
+		TranslateMessage (&msg) ;
+		DispatchMessage (&msg) ;
+
+		vaDbgTs(_T("#%d - MSG: %s <<<" ), s_count, wmstr.get());
+	}
+
+	return (int)msg.wParam; // the value N told by PostQuitMessage(N);
+}
+
+BOOL Cls_OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
+{
+	vaSetWindowText(hwnd, _T("tWinCursor1 v%s"), _T(EXE_VERSION));
+	return TRUE; // success, go on creation
+}
+
+void Cls_OnPaint(HWND hwnd)
+{
+	PAINTSTRUCT ps = {};
+	RECT        rect ;
+	HDC hdc = BeginPaint (hwnd, &ps) ;
+
+	GetClientRect (hwnd, &rect) ;          
+	Ellipse(hdc, 0,0, rect.right, rect.bottom);
+	DrawText (hdc, TEXT ("Hello, WindowsX !"), -1, &rect,
+		DT_SINGLELINE | DT_CENTER | DT_VCENTER) ;
+
+	EndPaint (hwnd, &ps) ;
+}
+
+void Cls_OnDestroy(HWND hwnd)
+{
+	vaDbgTs(_T("====WM_DESTROY===="));
+	PostQuitMessage(44);
+}
+
+void Cls_OnClose(HWND hwnd)
+{
+	vaDbgTs(_T("====WM_CLOSE===="));
+	FORWARD_WM_CLOSE(hwnd, DefWindowProc);
+}
+
+
+UINT Cls_OnNCHitTest(HWND hwnd, int x, int y)
+{
+	UINT htcode = FORWARD_WM_NCHITTEST(hwnd, x, y, DefWindowProc);
+
+	vaDbgTs(_T("WM_NCHITTEST [%d,%d] returns HT=%s"), x, y, ITCSv(htcode, HTxxx_HitTest));
+
+	return htcode;
+}
+
+BOOL Cls_OnSetCursor(HWND hwnd, HWND hwndCursor, UINT codeHitTest, UINT mousemsg)
+{
+	POINT pt = {};
+	BOOL succ = GetCursorPos(&pt);
+	assert(succ);
+
+	if(hwnd==hwndCursor)
+	{
+		vaDbgTs(_T("WM_SETCURSOR: HT=%s , mousemsg=%s [%d,%d]"), 
+			ITCSv(codeHitTest, HTxxx_HitTest), 
+			ITCSv(mousemsg, WM_xxx), 
+			pt.x, pt.y);
+	}
+	else
+	{
+		// PendingQ: When can I see this?
+		vaDbgTs(_T("WM_SETCURSOR[0x%08X]: HT=%s , mousemsg=%s [%d,%d]}"), 
+			hwndCursor, 
+			ITCSv(codeHitTest, HTxxx_HitTest), 
+			ITCSv(mousemsg, WM_xxx), 
+			pt.x, pt.y);
+	}
+
+	return FORWARD_WM_SETCURSOR(hwnd, hwndCursor, codeHitTest, mousemsg, DefWindowProc);
+}
+
+void Cls_OnLButtonDown(HWND hwnd, BOOL fDoubleClick, int x, int y, UINT keyFlags)
+{
+	vaDbgTs(_T("WM_LBUTTONDOWN keyFlags=0x%X"), keyFlags);
+	SetCursor(LoadCursor(NULL, IDC_SIZEALL));
+}
+
+void Cls_OnLButtonUp(HWND hwnd, int x, int y, UINT keyFlags)
+{
+	vaDbgTs(_T("WM_LBUTTONUP"));
+	//	SetCursor(LoadCursor(NULL, IDC_IBEAM));
+}
+
+
+void Cls_OnRButtonDown(HWND hwnd, BOOL fDoubleClick, int x, int y, UINT keyFlags)
+{
+	vaDbgTs(_T("WM_RBUTTONDOWN"));
+	//	SetCursor(LoadCursor(NULL, IDC_SIZEALL));
+
+	HWND hwndDesk = GetDesktopWindow();
+	printf("hwndDesktop = 0x%X\n", hwndDesk);
+
+	//	Sleep(1000);
+	//	SendMessage(hwndDesk, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+}
+
+
+void Cls_OnTimer(HWND hwnd, UINT id)
+{
+	int fMouse = GetSystemMetrics (SM_MOUSEPRESENT);
+	vaDbgTs(_T("[T1] SM_MOUSEPRESENT: %d"), fMouse);
+}
+
+void Cls_OnKey(HWND hwnd, UINT vk, BOOL fDown, int cRepeat, UINT flags)
+{
+	if(fDown)
+	{
+		Sleep(500);
+		vaDbgTs(_T("KEYDOWN repeat: %d"), cRepeat);
+	}
+}
+
+void Cls_OnNCLButtonDown(HWND hwnd, BOOL fDoubleClick, int x, int y, UINT codeHitTest)
+{
+	if(!fDoubleClick)
+	{
+		vaDbgTs(_T("Cls_OnNCLButtonDown HT=%s [%d,%d]"), 
+			ITCSv(codeHitTest, HTxxx_HitTest), 
+			x,y);
+	}
+	else
+	{
+		vaDbgTs(_T("Cls_OnNCLButtonDown HT=%s [%d,%d] (DB-click)"), 
+			ITCSv(codeHitTest, HTxxx_HitTest), 
+			x,y);
+	}
+	FORWARD_WM_NCLBUTTONDOWN(hwnd, fDoubleClick, x, y, codeHitTest, DefWindowProc);
+}
+
+void Cls_OnNCLButtonUp(HWND hwnd, int x, int y, UINT codeHitTest)
+{
+	vaDbgTs(_T("Cls_OnNCLButtonUp HT=%s [%d,%d]"), 
+		ITCSv(codeHitTest, HTxxx_HitTest), 
+		x,y);
+}
+
+void Cls_OnMouseMove(HWND hwnd, int x, int y, UINT keyFlags)
+{
+	vaDbgTs(_T("WM_MOUSEMOVE [%d,%d]"), x, y);
+}
+
+
+LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	
+	switch (message)
+	{{
+		HANDLE_MSG(hwnd, WM_CREATE, Cls_OnCreate);
+		HANDLE_MSG(hwnd, WM_PAINT, Cls_OnPaint);
+		HANDLE_MSG(hwnd, WM_DESTROY, Cls_OnDestroy);
+		HANDLE_MSG(hwnd, WM_CLOSE, Cls_OnClose);
+
+		HANDLE_MSG(hwnd, WM_LBUTTONDOWN, Cls_OnLButtonDown);
+		HANDLE_MSG(hwnd, WM_LBUTTONUP, Cls_OnLButtonUp);
+
+		HANDLE_MSG(hwnd, WM_NCHITTEST, Cls_OnNCHitTest); // highlight
+		HANDLE_MSG(hwnd, WM_SETCURSOR, Cls_OnSetCursor);
+
+		HANDLE_MSG(hwnd, WM_RBUTTONDOWN, Cls_OnRButtonDown);
+
+		HANDLE_MSG(hwnd, WM_TIMER, Cls_OnTimer);
+		HANDLE_MSG(hwnd, WM_KEYDOWN, Cls_OnKey);
+
+		HANDLE_MSG(hwnd, WM_NCLBUTTONDOWN, Cls_OnNCLButtonDown);
+		HANDLE_MSG(hwnd, WM_NCLBUTTONDBLCLK, Cls_OnNCLButtonDown);
+		HANDLE_MSG(hwnd, WM_NCLBUTTONUP, Cls_OnNCLButtonUp);
+
+		HANDLE_MSG(hwnd, WM_MOUSEMOVE, Cls_OnMouseMove);
+	}}
+	
+	return DefWindowProc (hwnd, message, wParam, lParam) ;
+}
