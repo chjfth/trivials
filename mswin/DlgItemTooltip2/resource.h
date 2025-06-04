521c7f03ba45d01018909774339e20b382342b02
chj Win7Evernote VM <chjfth@gmail.com>
2025-06-04 21:43:36 +0800

    DlgItemTooltip2

---
+++ mswin/DlgItemTooltip2/resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DlgItemTooltip2.rc
+//
+#define IDI_WINMAIN                     100
+#define IDD_WINMAIN                     101
+#define IDC_LABEL1                      1000
+#define IDC_EDIT_LOGMSG                 1001
+#define IDC_BUTTON1                     1002
+#define IDB_CreateTooltip               1003
+#define IDCK_WsExTransparent            1004
+#define IDCK_TtsTrans                   1005
+#define IDCK_TtfTransparent             1005
+#define IDCK_TTS_BALLOON                1006
+#define IDC_CHECK2                      1007
+#define IDCK_TTF_CENTERTIP              1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
