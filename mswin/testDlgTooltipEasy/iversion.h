72c59259042de77ebc979e769e26983d60ffdf11
chj Win7Evernote VM <chjfth@gmail.com>
2025-06-06 23:27:37 +0800

    temp doing DlgTooltipEasy

---
+++ mswin/testDlgTooltipEasy/iversion.h
@@ -0,0 +1,28 @@
+#ifndef __testDlgTooltipEasy_iversion_h_
+#define __testDlgTooltipEasy_iversion_h_
+
+#define testDlgTooltipEasy_VMAJOR 1
+#define testDlgTooltipEasy_VMINOR 0
+#define testDlgTooltipEasy_VPATCH 0
+#define testDlgTooltipEasy_VTAIL  1
+
+#define testDlgTooltipEasystr__(n) #n
+#define testDlgTooltipEasystr(n) testDlgTooltipEasystr__(n)
+
+// The following 4 are used in .rc
+#define testDlgTooltipEasy_VMAJORs testDlgTooltipEasystr(testDlgTooltipEasy_VMAJOR)
+#define testDlgTooltipEasy_VMINORs testDlgTooltipEasystr(testDlgTooltipEasy_VMINOR)
+#define testDlgTooltipEasy_VPATCHs testDlgTooltipEasystr(testDlgTooltipEasy_VPATCH)
+#define testDlgTooltipEasy_VTAILs  testDlgTooltipEasystr(testDlgTooltipEasy_VTAIL)
+
+#define testDlgTooltipEasy_NAME "testDlgTooltipEasy"
+
+enum {
+	testDlgTooltipEasy_vmajor = testDlgTooltipEasy_VMAJOR,
+	testDlgTooltipEasy_vminor = testDlgTooltipEasy_VMINOR,
+	testDlgTooltipEasy_vpatch = testDlgTooltipEasy_VPATCH,
+	testDlgTooltipEasy_vtail = testDlgTooltipEasy_VTAIL,
+};
+
+
+#endif
