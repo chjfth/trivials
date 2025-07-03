44954917829f71edde9f9a7f726357237998d6c6
chj Win7Evernote VM <chjfth@gmail.com>
2025-07-03 19:03:33 +0800

    Adjust CHHI lib's order for better doc illustration.

--- mswin/testWinMultiMon/CHHI.cpp
+++ mswin/testWinMultiMon/CHHI.cpp
@@ -2,12 +2,12 @@
 #include "../utils.h" // provides vaDbgTs() declaration
 #include <CHHI_vaDBG_is_vaDbgTs.h> // use vaDbgTs() inside
 
-#define JULayout2_IMPL
-#include <mswin/JULayout2.h>
-
 #define JAutoBuf_IMPL
 #include <JAutoBuf.h>
 
 #define WinMultiMon_IMPL
 #define WinMultiMon_DEBUG
 #include <mswin/WinMultiMon.h>
+
+#define JULayout2_IMPL
+#include <mswin/JULayout2.h>
