4ee5759bfccb5e4a4cb95ae2eb95424d84355fc1
chj Win7Evernote VM <chjfth@gmail.com>
2026-05-20 09:27:35 +0800

    Add test_enumor_helper.cpp . MSVC run OK, but `gcc -O2` crashes.

--- chjds/test_enumor_helper/test_enumor_helper.cpp
+++ chjds/test_enumor_helper/test_enumor_helper.cpp
@@ -1,22 +1,81 @@
+#undef NDEBUG // always enable assert()
+
 #include <stdio.h>
 #include <assert.h>
 #include <locale.h>
 #include <ps_TCHAR.h>
+#include <commdefs.h>
 #include <msvc_extras.h>
 #include <cxx_stack.h>
 	using namespace chjds;
 #include <Sdring.h>
 
+#define LOCK_YES(stk) ((stk).dbg_peek_enumlocks()>0)
+#define LOCK_NO(stk) ((stk).dbg_peek_enumlocks()==0)
 
-void test0()
+void test_enumor_helper()
 {
 	// Create a custom stack, so that we can enumerate elements from that stack.
 
-	stack<Sdring> stk1;
-	stk1.push(_T("ele0"));
-	stk1.push(_T("ele1"));
-	stk1.push(_T("ele2"));
+	TCHAR const* const artext[] = {_T("ele0"), _T("ele1"), _T("ele2") };
+	const int ncount = ARRAY_SIZE(artext);
+	int i, idx=0;
+	Sdring *ps = nullptr;
 
+	stack<Sdring> stk1;
+	for(i=0; i<ncount; i++)
+		stk1.push( artext[i] );
+
+	stack<Sdring> stk2 = stk1;
+
+	auto en1 = stk1.get_enumor();
+	assert(LOCK_NO(stk1));
+
+	for(i=0; ;i++)
+	{
+		idx = en1.next(&ps);
+		if(idx==-1)
+			break;
+
+		assert( idx == i );
+		assert( Sdring::str_match(artext[i], ps->c_str()) );
+
+		assert(LOCK_YES(stk1));
+	}
+	assert(i == ncount);
+	assert(LOCK_NO(stk1));
+
+	//
+	// Test about reset() and lock-state.
+	//
+
+	en1.reset();
+	assert(LOCK_NO(stk1));
+
+	idx = en1.next(&ps);
+	assert(idx==0 && Sdring::str_match(artext[0], ps->c_str()));
+	assert(LOCK_YES(stk1));
+
+	// We can reset() in midway, and lock-state is free-d.
+	en1.reset();
+	assert(LOCK_NO(stk1));
+
+	//
+	// Verify again.
+	//
+	for(i=0; ;i++)
+	{
+		idx = en1.next(&ps);
+		if (idx == -1)
+			break;
+
+		assert(idx == i);
+		assert(Sdring::str_match(artext[i], ps->c_str()));
+
+		assert(LOCK_YES(stk1));
+	}
+	assert(i == ncount);
+	assert(LOCK_NO(stk1));
 }
 
 
@@ -31,7 +90,7 @@ int _tmain(int argc, TCHAR *argv[])
 #endif
 
 
-	test0();
+	test_enumor_helper();
 
 #ifdef _MSC_VER
 	// Take snapshot after
